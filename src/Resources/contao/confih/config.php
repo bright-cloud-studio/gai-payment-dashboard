8e4ef387860dee282bf2845f216bf60b1160a0f1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-07 15:09:40 -0400

    Create config.php

---
+++ src/Resources/contao/confih/config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    GAI Payment Dashboard
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/gai-payment-d
+*/
+
+use Bcs\PaymentDashboardBundle\FormRecaptcha;
