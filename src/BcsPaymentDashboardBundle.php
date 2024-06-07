e9a171f6ec8b3b15a2dd9e7066221b91cba7ec97
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-07 15:06:17 -0400

    Create BcsPaymentDashboardBundle.php

---
+++ src/BcsPaymentDashboardBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    GAI Payment Dashboard
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-recaptcha
+ */
+
+namespace Bcs\PaymentDashboardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsPaymentDashboardBundle extends Bundle
+{
+}
