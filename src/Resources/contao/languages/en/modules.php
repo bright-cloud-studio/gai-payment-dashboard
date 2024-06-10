c0c3ad42afc861cba487310d7b29562520005e46
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-10 09:46:48 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Payment Dashboard
+ *
+ * Copyright (C) 2024-2025 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-payment-dashboard
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Back end modules */
+$GLOBALS['TL_LANG']['MOD']['transactions']                  = array('Transactions', 'Stored Transactions for the Duplicate Checker');
