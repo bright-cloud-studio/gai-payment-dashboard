0299196908af1277ad92767d9e132288dbe6b930
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-10 10:08:41 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -7,8 +7,3 @@
 * @license    LGPL-3.0+
 * @see	       https://github.com/bright-cloud-studio/gai-payment-d
 */
-
-/* Back end modules */
-$GLOBALS['BE_MOD']['content']['transaction'] = array(
-	'tables' => array('tl_transaction')
-);
