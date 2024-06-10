8b4c031311a958a9956111da84165860716e4378
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-10 09:43:43 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -8,4 +8,7 @@
 * @see	       https://github.com/bright-cloud-studio/gai-payment-d
 */
 
-use Bcs\PaymentDashboardBundle\FormRecaptcha;
+/* Back end modules */
+$GLOBALS['BE_MOD']['content']['transactions'] = array(
+	'tables' => array('tl_transaction')
+);
