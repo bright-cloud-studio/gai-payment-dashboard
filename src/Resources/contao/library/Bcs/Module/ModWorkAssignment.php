cf7c4bb555ec58d6af189e80eafc5f63801c8011
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-18 13:29:22 -0400

    Create ModWorkAssignment.php

---
+++ src/Resources/contao/library/Bcs/Module/ModWorkAssignment.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* Bright Cloud Studio's GAI Payment Dashboard
+*
+* Copyright (C) 2024-2025 Bright Cloud Studio
+*
+* @package    bright-cloud-studio/gai-payment-dashboard
+* @link       https://www.brightcloudstudio.com/
+* @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+class ModWorkAssignment extends \Contao\Module
+{
+
+
+  
+
+}
