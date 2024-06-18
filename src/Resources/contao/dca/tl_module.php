4e64f26115a91c3384d34fa9b2fd85d3d0421db2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-18 15:28:27 -0400

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Payment Dashboard
+ *
+ * Copyright (C) 2024-2025 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-payment-dashboard
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Module Palettes */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_work_assignment'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
