1f088477c68c65afbda2425dc7408e9321483574
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-21 14:50:33 -0400

    Create tl_district.php

---
+++ src/Resources/contao/languages/en/tl_district.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_district']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_district']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_district']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_district']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_district']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_district']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_district']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_district']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_district']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_district']['district_legend']            = 'Disctirct Information';
+$GLOBALS['TL_LANG']['tl_district']['date']                       = array('Date', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_district']['psychologist']               = array('Psychologist', 'Psycholigist this transactions is for');
+$GLOBALS['TL_LANG']['tl_district']['district']                   = array('District', 'District the school is in');
+$GLOBALS['TL_LANG']['tl_district']['school']                     = array('School', 'The name of the school');
+$GLOBALS['TL_LANG']['tl_district']['student_name']               = array('Student Name', 'The initials of the student');
+$GLOBALS['TL_LANG']['tl_district']['service_provided']           = array('Service Provided', 'The ID of the service being provided');
+$GLOBALS['TL_LANG']['tl_district']['price']                      = array('Price', 'Price for this transaction');
+$GLOBALS['TL_LANG']['tl_district']['lasid']                      = array('LASID', 'The LASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_district']['sasid']                      = array('SASID', 'The SASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_district']['meeting_date']               = array('Meeting Date', 'The date the meeting occured');
+$GLOBALS['TL_LANG']['tl_district']['meeting_start']              = array('Meeting Start', 'The time the meeting started');
+$GLOBALS['TL_LANG']['tl_district']['meeting_end']                = array('Meeting End', 'The time the meeting ended');
+$GLOBALS['TL_LANG']['tl_district']['meeting_duration']           = array('Meeting Duration', 'The amount of minutes the meeting lasted for');
+$GLOBALS['TL_LANG']['tl_district']['notes']                      = array('Notes', 'Notes added by the psychologist');
+$GLOBALS['TL_LANG']['tl_district']['internal_legend']            = 'FOR INTERNAL USE ONLY';
+$GLOBALS['TL_LANG']['tl_district']['reviewed']                   = array('Reviewed', 'Tracks if the psychologist has performed their mandatory review');
+$GLOBALS['TL_LANG']['tl_district']['deleted']                    = array('Deleted', 'Tracks if the psychologist has deleted this transactions');
+$GLOBALS['TL_LANG']['tl_district']['misc_billing']               = array('Misc. Billing', 'The display name for a Misc. Billing transaction');
+$GLOBALS['TL_LANG']['tl_district']['sheet_row']                  = array('Sheet Row', 'The row of the Work Assignment this transaction was generated for');
+$GLOBALS['TL_LANG']['tl_district']['label']                      = array('Label', 'The label used as the main descriptor for Misc. Billing Transactions');
