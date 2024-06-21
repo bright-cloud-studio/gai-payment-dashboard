be43bcac6f58efda2b6bab9b643d8b9c640b0d08
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-21 14:51:20 -0400

    Create tl_school.php

---
+++ src/Resources/contao/languages/en/tl_school.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_school']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_school']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_school']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_school']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_school']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_school']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_school']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_school']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_school']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_school']['school_legend']            = 'School Information';
+$GLOBALS['TL_LANG']['tl_school']['date']                       = array('Date', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_school']['psychologist']               = array('Psychologist', 'Psycholigist this transactions is for');
+$GLOBALS['TL_LANG']['tl_school']['district']                   = array('District', 'District the school is in');
+$GLOBALS['TL_LANG']['tl_school']['school']                     = array('School', 'The name of the school');
+$GLOBALS['TL_LANG']['tl_school']['student_name']               = array('Student Name', 'The initials of the student');
+$GLOBALS['TL_LANG']['tl_school']['service_provided']           = array('Service Provided', 'The ID of the service being provided');
+$GLOBALS['TL_LANG']['tl_school']['price']                      = array('Price', 'Price for this transaction');
+$GLOBALS['TL_LANG']['tl_school']['lasid']                      = array('LASID', 'The LASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_school']['sasid']                      = array('SASID', 'The SASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_school']['meeting_date']               = array('Meeting Date', 'The date the meeting occured');
+$GLOBALS['TL_LANG']['tl_school']['meeting_start']              = array('Meeting Start', 'The time the meeting started');
+$GLOBALS['TL_LANG']['tl_school']['meeting_end']                = array('Meeting End', 'The time the meeting ended');
+$GLOBALS['TL_LANG']['tl_school']['meeting_duration']           = array('Meeting Duration', 'The amount of minutes the meeting lasted for');
+$GLOBALS['TL_LANG']['tl_school']['notes']                      = array('Notes', 'Notes added by the psychologist');
+$GLOBALS['TL_LANG']['tl_school']['internal_legend']            = 'FOR INTERNAL USE ONLY';
+$GLOBALS['TL_LANG']['tl_school']['reviewed']                   = array('Reviewed', 'Tracks if the psychologist has performed their mandatory review');
+$GLOBALS['TL_LANG']['tl_school']['deleted']                    = array('Deleted', 'Tracks if the psychologist has deleted this transactions');
+$GLOBALS['TL_LANG']['tl_school']['misc_billing']               = array('Misc. Billing', 'The display name for a Misc. Billing transaction');
+$GLOBALS['TL_LANG']['tl_school']['sheet_row']                  = array('Sheet Row', 'The row of the Work Assignment this transaction was generated for');
+$GLOBALS['TL_LANG']['tl_school']['label']                      = array('Label', 'The label used as the main descriptor for Misc. Billing Transactions');
