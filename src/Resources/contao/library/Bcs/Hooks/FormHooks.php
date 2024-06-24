b68bb61e2786fc228d1db59a78a0f26834ac28fa
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-24 15:10:11 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -5,18 +5,24 @@ namespace Bcs\Hooks;
 class FormHooks
 {
     protected static $arrUserOptions = array();
- 
+
+    // When a form is submitted
     public function onFormSubmit($submittedData, $formData, $files, $labels, $form)
     {
+        // If this is our specific form
         if($formData['formID'] == 'work_assignment') {
-            echo "HOOK HIT1!";
-            die();
+
+            // Create transaction using submitted data
         }
     }
 
+    // When a form is loaded
     public function onPrepareForm($fields, $formId, $form)
     {
+        // If this is our specific form
         if($form->formID == 'work_assignment') {
+
+            // Prefill in our Work Assignment information
             return;
         }
 
