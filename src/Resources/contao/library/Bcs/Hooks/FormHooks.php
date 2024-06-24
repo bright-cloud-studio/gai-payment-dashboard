efaca873dd865d58afc51b3991fc8c88190c7335
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-24 14:34:10 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -9,7 +9,15 @@ class FormHooks
     public function onFormSubmit($submittedData, $formData, $files, $labels, $form)
     {
         if($formData['formID'] == 'work_assignment') {
-            echo "HOOK HIT!";
+            echo "HOOK HIT1!";
+            die();
+        }
+    }
+
+    public function onPrepareForm($fields, $formId, $form)
+    {
+        if($formData['formID'] == 'work_assignment') {
+            echo "HOOK HIT2!";
             die();
         }
     }
