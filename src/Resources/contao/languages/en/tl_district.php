0710e2d503954451687990db9d29c1d5a3d0d9a2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-06-25 14:14:07 -0400

    Update tl_district.php

--- src/Resources/contao/languages/en/tl_district.php
+++ src/Resources/contao/languages/en/tl_district.php
@@ -24,24 +24,8 @@ $GLOBALS['TL_LANG']['tl_district']['alias']                      = array('Alias'
 $GLOBALS['TL_LANG']['tl_district']['published']                  = array('Published', 'Show this record on the front end.');
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_district']['district_legend']                    = 'Invoice Specific';
-$GLOBALS['TL_LANG']['tl_district']['invoice_number']                     = array('Invoice Number', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['invoice_prefix']                     = array('Invoice Prefix', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['invoice_purchase_order']             = array('Invoice Purchase Order', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['invoice_price_tier']                 = array('Invoice Price Tier', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['invoice_last_generation_date']       = array('Invoice Last Generation Date', 'Today\'s Date');
-
-
-$GLOBALS['TL_LANG']['tl_district']['district_legend']                    = 'District Specific';
-$GLOBALS['TL_LANG']['tl_district']['district_name']                      = array('District Name', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['district_address']                   = array('District Address', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['district_city']                      = array('District City', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['district_state']                     = array('District State', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['district_zip']                       = array('District Zip', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['district_phone']                     = array('District Phone', 'Today\'s Date');
-
-
-$GLOBALS['TL_LANG']['tl_district']['district_legend']                    = 'Contact Specific';
-$GLOBALS['TL_LANG']['tl_district']['contact_name']                       = array('Contact Name', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['contact_email']                      = array('Contact Email', 'Today\'s Date');
-$GLOBALS['TL_LANG']['tl_district']['contact_cc_email']                   = array('Contact CC Email', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_district']['district_legend']            = 'Invoice Specific';
+$GLOBALS['TL_LANG']['tl_district']['invoice_number']             = array('Invoice Number', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_district']['invoice_prefix']             = array('Invoice Prefix', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_district']['purchase_order']             = array('Purchase Order', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_district']['price_tier']                 = array('Price Tier', 'Today\'s Date');
