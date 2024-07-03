711a901233aedcfd81e65bcf33fd0a64716b979f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-03 13:58:29 -0400

    Update tl_transaction.php

--- src/Resources/contao/languages/en/tl_transaction.php
+++ src/Resources/contao/languages/en/tl_transaction.php
@@ -25,7 +25,7 @@ $GLOBALS['TL_LANG']['tl_transaction']['published']                  = array('Pub
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_transaction']['transaction_legend']         = 'Transaction Information';
-$GLOBALS['TL_LANG']['tl_transaction']['date']                       = array('Date', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction']['date_submitted']             = array('Date Submitted', 'asdf');
 $GLOBALS['TL_LANG']['tl_transaction']['psychologist']               = array('Psychologist', 'asdf');
 $GLOBALS['TL_LANG']['tl_transaction']['district']                   = array('District', 'asdf');
 $GLOBALS['TL_LANG']['tl_transaction']['school']                     = array('School', 'asdf');
@@ -39,4 +39,4 @@ $GLOBALS['TL_LANG']['tl_transaction']['meeting_start']              = array('Mee
 $GLOBALS['TL_LANG']['tl_transaction']['meeting_end']                = array('Meeting End', 'asdf');
 $GLOBALS['TL_LANG']['tl_transaction']['meeting_duration']           = array('Meeting Duration', 'asdf');
 $GLOBALS['TL_LANG']['tl_transaction']['notes']                      = array('Notes', 'asdf');
-
+$GLOBALS['TL_LANG']['tl_transaction']['reviewed']                   = array('Reviewed', 'asdf');
