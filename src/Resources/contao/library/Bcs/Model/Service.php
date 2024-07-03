64b3987255482cbf5c347e33e18ac5346f0885fe
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-03 08:54:52 -0400

    Create Service.php

---
+++ src/Resources/contao/library/Bcs/Model/Service.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+
+class Service extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_service';
+    
+}
