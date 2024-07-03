1a65bea872c9d21f73dd5f931d8e47e734f0a618
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-03 09:46:03 -0400

    Create tl_service.php

---
+++ src/Resources/contao/languages/en/tl_service.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_service']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_service']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_service']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_service']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_service']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_service']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_service']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_service']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_service']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_service']['services_legend']         = 'Service Information';
+$GLOBALS['TL_LANG']['tl_service']['service_code']            = array('Service Code', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['name']                    = array('Name', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_1']     = array('Psychologist Tier 1', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_2']     = array('Psychologist Tier 2', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_3']     = array('Psychologist Tier 3', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_4']     = array('Psychologist Tier 4', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_5']     = array('Psychologist Tier 5', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_6']     = array('Psychologist Tier 6', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_7']     = array('Psychologist Tier 7', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_8']     = array('Psychologist Tier 8', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_9']     = array('Psychologist Tier 9', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['psychologist_tier_10']    = array('Psychologist Tier 10', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['school_tier_1']           = array('School Tier 1', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['school_tier_2']           = array('School Tier 2', 'asdf');
+$GLOBALS['TL_LANG']['tl_service']['school_tier_3']           = array('School Tier 3', 'asdf');
