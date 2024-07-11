e34814337f0736525c25ada99059396eb9a75a22
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-11 11:51:32 -0400

    Create InvoiceRequest.php

---
+++ src/Resources/contao/library/Bcs/Model/InvoiceRequest.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+
+class InvoiceRequest extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_invoice_request';
+    
+}
