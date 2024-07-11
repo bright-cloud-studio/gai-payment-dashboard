ca58da172e5ae09a32f0094b26e012acfb8d7db7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-11 11:50:59 -0400

    Create tl_invoice_request.php

---
+++ src/Resources/contao/languages/en/tl_invoice_request.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_invoice_request']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_invoice_request']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_request']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_request']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_request']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_request']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_invoice_request']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_invoice_request']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_invoice_request']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_invoice_request']['invoice_request_legend']        = 'Invoice Request Information';
+$GLOBALS['TL_LANG']['tl_invoice_request']['date_start']                    = array('Date Range - Start', 'asdf');
+$GLOBALS['TL_LANG']['tl_invoice_request']['date_end']                      = array('Date Range - End', 'asdf');
+
+$GLOBALS['TL_LANG']['tl_invoice_request']['exclude_psychologists']          = array('Exclude Psychologists', 'asdf');
+$GLOBALS['TL_LANG']['tl_invoice_request']['exclude_districts']              = array('Exclude Districts', 'asdf');
