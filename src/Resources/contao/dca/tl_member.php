48446cb38b1f5e950a0e6529880cb45a49a852c3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-15 14:23:38 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -10,12 +10,13 @@ foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
 }
     
 /* Add fields to tl_user */
-$GLOBALS['TL_DCA']['tl_member']['fields']['meeting_options'] = array
+$GLOBALS['TL_DCA']['tl_member']['fields']['price_tier_assignments'] = array
 (
-    'sql'                     => "varchar(255) NOT NULL default ''",
-    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['meeting_options'],
+    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['price_tier_assignments'],
     'inputType'               => 'checkbox',
-    'options'                 => array('misc_billing' => 'Misc. Billing', 'misc_travel_expenses' => 'Misc. Travel Expenses', 'editing_services' => 'Editing Services', 'manager' => 'Manager', 'parking' => 'Parking', 'test_late_cancel_first' => 'First Test Late Cancel', 'test_late_cancel_additional' => 'Additional Test Late Cancel'),								
-    'eval'                    => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'clr') 
+    'flag'                    => DataContainer::SORT_ASC,
+    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    'options_callback'	      => array('Bcs\Backend\ServiceBackend', 'getPriceTierAssignmentOptions'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
 
