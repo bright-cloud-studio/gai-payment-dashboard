069080fabb586d7c00e5c9a273a92b7858a53798
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-15 14:30:30 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -8,7 +8,7 @@ use Contao\DC_Table;
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_options;{price_tier_legend},price_tier;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},price_tier_assignments;{price_tier_legend},price_tier;', $v);
 }
     
 /* Add fields to tl_user */
