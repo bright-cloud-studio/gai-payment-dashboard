3b3a80ecc6531cf56f2eb0de06c9ffc83d498a20
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-15 13:38:45 -0400

    Create PriceTier.php

---
+++ src/Resources/contao/library/Bcs/Model/PriceTier.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+
+class PriceTier extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_price_tier';
+    
+}
