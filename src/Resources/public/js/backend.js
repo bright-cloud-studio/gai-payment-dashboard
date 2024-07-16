efbfe1ff3a4bbcddad6a476e4c15a8a33447af9d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-16 11:42:33 -0400

    Update backend.js

--- src/Resources/public/js/backend.js
+++ src/Resources/public/js/backend.js
@@ -1,9 +1,9 @@
 <script>
 
-	$(document).ready(function() {
+    $(document).ready(function() {
     
-    console.log("BACKEND LOADED");
+        console.log("BACKEND LOADED");
     
-	});
-	
+    });
+
 </script>
