11da094d3e8771c90d8c348ef83b74900861f92f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-18 14:50:12 -0400

    Create tl_student.php

---
+++ src/Resources/contao/languages/en/tl_student.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_student']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_student']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_student']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_student']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_student']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_student']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_student']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_student']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_student']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_student']['assignment_legend']  = 'Student Information';
+$GLOBALS['TL_LANG']['tl_student']['name']               = array('Name', 'asdf');
+$GLOBALS['TL_LANG']['tl_student']['date_of_birth']      = array('Date of Birth', 'asdf');
+$GLOBALS['TL_LANG']['tl_student']['gender']             = array('Gender', 'asdf');
+$GLOBALS['TL_LANG']['tl_student']['grade']              = array('Grade', 'asdf');
+$GLOBALS['TL_LANG']['tl_student']['lasid']              = array('LASID', 'asdf');
+$GLOBALS['TL_LANG']['tl_student']['sasid']              = array('SASID', 'asdf');
