9942c17054992fdfe25bacd44cc2dfe5a31f4f77
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-18 13:03:48 -0400

    Update backend.js

--- src/Resources/public/js/backend.js
+++ src/Resources/public/js/backend.js
@@ -1,7 +1,15 @@
+/* Once fully loaded */
 $(document).ready(function() {
 
-    $(document).ready(function() {
-        $('#ctrl_psychologist').select2();
-    });
+    /* Apply Select2 - Assignments */
+    $(document).ready(function(){ $('#ctrl_psychologist').select2(); });
+    $(document).ready(function(){ $('#ctrl_district').select2(); });
+    $(document).ready(function(){ $('#ctrl_school').select2(); });
+    $(document).ready(function(){ $('#ctrl_student_gender').select2(); });
+
+    $(document).ready(function(){ $('#ctrl_initial_reeval').select2(); });
+    $(document).ready(function(){ $('#ctrl_type_of_testing').select2(); });
+    $(document).ready(function(){ $('#ctrl_meeting_required').select2(); });
+    $(document).ready(function(){ $('#ctrl_report_submitted').select2(); });
 
 });
