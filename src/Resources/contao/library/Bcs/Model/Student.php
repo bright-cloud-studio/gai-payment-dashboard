c5e0ae302064b158385222b420d4c1f2af734b28
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-18 14:37:46 -0400

    Create Student.php

---
+++ src/Resources/contao/library/Bcs/Model/Student.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+class Student extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_student';
+    
+}
