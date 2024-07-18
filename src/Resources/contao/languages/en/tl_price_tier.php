a82bae02aee0ba5bfd1393f3a9fe6e9493bf59fa
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-18 15:02:16 -0400

    Create tl_price_tier.php

---
+++ src/Resources/contao/languages/en/tl_price_tier.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_price_tier']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_price_tier']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_price_tier']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_price_tier']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_price_tier']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_price_tier']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_price_tier']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_price_tier']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_price_tier']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_price_tier']['price_tier_legend']  = 'Price Tier Information';
+$GLOBALS['TL_LANG']['tl_price_tier']['tier_price']         = array('Tier Price', 'asdf');
