c726331e53dccd963544e10ab808636bdb137726
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 15:49:30 -0400

    Update PriceTierBackend.php

--- src/Resources/contao/library/Bcs/Backend/PriceTierBackend.php
+++ src/Resources/contao/library/Bcs/Backend/PriceTierBackend.php
@@ -54,7 +54,7 @@ class PriceTierBackend extends Backend
 
 		// Update the database
 		$this->Database->prepare("UPDATE tl_price_tier SET tstamp=". time() .", published='" . ($blnVisible ? 1 : '') . "' WHERE id=?")->execute($intId);
-		$this->log('A new version of record "tl_price_tier.id='.$intId.'" has been created'.$this->getParentEntries('tl_listing', $intId), __METHOD__, TL_GENERAL);
+		//$this->log('A new version of record "tl_price_tier.id='.$intId.'" has been created'.$this->getParentEntries('tl_listing', $intId), __METHOD__, TL_GENERAL);
 	}
 	
 	
