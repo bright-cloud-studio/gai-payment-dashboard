0cc2be5000bffa9adac78557e4fd478ee22ed258
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-31 11:48:26 -0400

    Update tl_service.php

--- src/Resources/contao/dca/tl_service.php
+++ src/Resources/contao/dca/tl_service.php
@@ -151,7 +151,7 @@ $GLOBALS['TL_DCA']['tl_service'] = array
             'filter'                  => true,
             'search'                  => true,
             'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
+            'sql'                     => "int(5) NOT NULL default ''"
         ),
         'name' => array
         (
