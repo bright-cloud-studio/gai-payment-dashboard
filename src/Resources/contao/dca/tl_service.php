cccdf5ddd94f106309d5ea8edd6c1adba8607529
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-02 15:11:02 -0400

    Update tl_service.php

--- src/Resources/contao/dca/tl_service.php
+++ src/Resources/contao/dca/tl_service.php
@@ -36,7 +36,7 @@ $GLOBALS['TL_DCA']['tl_service'] = array
     (
         'sorting' => array
         (
-             'mode'                    => DataContainer::MODE_SORTED,
+            'mode'                    => DataContainer::MODE_SORTED,
             'rootPaste'               => false,
             'showRootTrails'          => false,
             'icon'                    => 'pagemounts.svg',
@@ -150,7 +150,7 @@ $GLOBALS['TL_DCA']['tl_service'] = array
             'filter'                  => true,
             'search'                  => true,
             'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
-            'sql'                     => "int(5) unsigned NOT NULL default ''"
+            'sql'                     => "int(5) unsigned NOT NULL default '0'"
         ),
         'name' => array
         (
