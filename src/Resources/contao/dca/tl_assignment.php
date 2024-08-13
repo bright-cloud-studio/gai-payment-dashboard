fc21b9cfdb6e7a22bb55398366778eaa592b0071
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-13 09:08:25 -0400

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -215,7 +215,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'filter'                  => true,
             'search'                  => true,
             'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
             'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getSchools'),
             'sql'                     => "varchar(255) NOT NULL default ''"
         ),
