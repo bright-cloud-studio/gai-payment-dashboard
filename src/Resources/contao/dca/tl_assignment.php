fe573427e5800c32851bc09d9a0ef1c2769b5ebf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-20 10:57:46 -0400

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -167,9 +167,8 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'eval'                    => array('datepicker'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
             'sql'                     => "varchar(20) NOT NULL default ''",
             'default'                 => date("m/d/y g:i A"),
-        )
+        ),
         /*
-        ,
         'date_30_day' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['date_30_day'],
