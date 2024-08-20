dcada3000388346b474b0cfac8d7b8432aa76534
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-20 11:31:35 -0400

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -206,7 +206,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'filter'                  => true,
             'search'                  => true,
             'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true, 'submitOnChange'=>true),
             'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getDistricts'),
             'sql'                     => "varchar(255) NOT NULL default ''"
         ),
