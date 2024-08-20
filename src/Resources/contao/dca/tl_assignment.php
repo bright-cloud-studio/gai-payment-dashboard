a0a3e3a7975adde654c7063fa31ed786422bf2f2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-20 10:46:28 -0400

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -113,7 +113,8 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
     // Palettes
     'palettes' => array
     (
-        'default'                     => '{assignment_legend}, date_created, date_30_day, date_45_day, psychologist, district, school, student, initial_reeval, type_of_testing, testing_date, meeting_required, meeting_date, contact_info_parent, contact_info_teacher, team_chair, email, report_submitted, notes;{publish_legend},published;'
+        'default'                     => '{assignment_legend}, date_created;'
+        //'default'                     => '{assignment_legend}, date_created, date_30_day, date_45_day, psychologist, district, school, student, initial_reeval, type_of_testing, testing_date, meeting_required, meeting_date, contact_info_parent, contact_info_teacher, team_chair, email, report_submitted, notes;{publish_legend},published;'
     ),
  
     // Fields
