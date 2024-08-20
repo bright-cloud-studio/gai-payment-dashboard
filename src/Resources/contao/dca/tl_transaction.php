2fa4b00412eb2252739c37064293bff08d490c0a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-20 12:12:00 -0400

    Update tl_transaction.php

--- src/Resources/contao/dca/tl_transaction.php
+++ src/Resources/contao/dca/tl_transaction.php
@@ -44,7 +44,9 @@ $GLOBALS['TL_DCA']['tl_transaction'] = array
     (
         'sorting' => array
         (
-            'mode'                    => DataContainer::MODE_TREE_EXTENDED,
+            'mode'                    => DataContainer::MODE_PARENT,
+            'headerFields'            => array('district', 'school', 'student', 'initial_reeval'),
+            
             'rootPaste'               => false,
 			
             'icon'                    => 'pagemounts.svg',
