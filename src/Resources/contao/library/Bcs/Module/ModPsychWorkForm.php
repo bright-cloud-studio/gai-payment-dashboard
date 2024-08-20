6bffc5615620dcd63e70c218c0a6de658139046f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-20 15:14:44 -0400

    Update ModPsychWorkForm.php

--- src/Resources/contao/library/Bcs/Module/ModPsychWorkForm.php
+++ src/Resources/contao/library/Bcs/Module/ModPsychWorkForm.php
@@ -65,7 +65,7 @@ class ModPsychWorkForm extends \Contao\Module
         $member = FrontendUser::getInstance();
 
         // Get Transactions that have our selected Assignment as the parent and that belong to this Psychologist
-        $transactions = Transaction::findBy(['pid = ?', 'psychologist = ?'], [$_SESSION['assignment_uuid'], $member->id]);
+        $transactions = Transaction::findBy(['psychologist = ?'], [$member->id]);
         
         foreach($transactions as $transaction) {
             $template_transactions[$transaction->id]['date_submitted'] = $transaction->date_submitted;
