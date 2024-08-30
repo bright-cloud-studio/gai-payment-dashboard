b78b7d9b6fbe5b597af834f2ee7a619a0d83401c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-30 11:02:15 -0400

    Create TransactionMisc.php

---
+++ src/Resources/contao/library/Bcs/Model/TransactionMisc.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+class TransactionMisc extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_transaction_misc';
+    
+}
