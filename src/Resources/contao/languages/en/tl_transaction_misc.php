c14f3923510d1686c4632582b4a4d342755bce61
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-30 11:00:01 -0400

    Create tl_transaction_misc.php

---
+++ src/Resources/contao/languages/en/tl_transaction_misc.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_transaction_misc']['new']                   = array('New Transaction', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['show']                       = array('Transaction Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['edit']                       = array('Edit Transaction', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['copy']                       = array('Copy Transaction', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['delete']                     = array('Delete Transaction', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['toggle']                     = array('Toggle Transaction', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_transaction_misc']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['published']                  = array('Reviewed', 'Indicates if this Transaction has been Reviewed by the submitting Psychologist');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_transaction_misc']['transaction_legend']         = 'Transaction Information';
+$GLOBALS['TL_LANG']['tl_transaction_misc']['date_submitted']             = array('Date Submitted', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['psychologist']               = array('Psychologist', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['service']                    = array('Service', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['price']                      = array('Price', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['meeting_legend']             = 'Meeting Information';
+$GLOBALS['TL_LANG']['tl_transaction_misc']['meeting_date']               = array('Meeting Date', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['meeting_start']              = array('Meeting Start', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['meeting_end']                = array('Meeting End', 'asdf');
+$GLOBALS['TL_LANG']['tl_transaction_misc']['meeting_duration']           = array('Meeting Duration', 'asdf');
+
+$GLOBALS['TL_LANG']['tl_transaction_misc']['notes_legend']                = 'Notes';
+$GLOBALS['TL_LANG']['tl_transaction_misc']['notes']                      = array('Notes', 'asdf');
+
+$GLOBALS['TL_LANG']['tl_transaction_misc']['publish_legend']             = 'Reviewed Information';
+$GLOBALS['TL_LANG']['tl_transaction_misc']['reviewed']                   = array('Reviewed', 'asdf');
