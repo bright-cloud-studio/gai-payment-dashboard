e8d44f0812acb8df041760023d878ab860cb8956
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-06 09:20:12 -0400

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -347,7 +347,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'default'                 => '',
             'search'                  => true,
             'eval'                    => array('mandatory'=>false, 'tl_class'=>'clr w100'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
+            'sql'                     => "text NOT NULL default ''"
         ),
         
         'published' => array
