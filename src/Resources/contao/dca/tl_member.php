d38ef44df7ccb2f42fca1646c3715a45679714ef
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-06 14:13:56 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -8,7 +8,7 @@ use Contao\DC_Table;
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend}, price_tier, price_tier_display;{price_tier_legend},price_tier;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend}, price_tier, price_tier_display;', $v);
 }
     
 /* Add fields to tl_user */
@@ -28,7 +28,7 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['price_tier_display'] = array
     'label'                   => &$GLOBALS['TL_LANG']['tl_member']['price_tier_display'],
     'inputType'               => 'checkbox',
     'flag'                    => DataContainer::SORT_ASC,
-    'eval'                    => array('mandatory'=>false, 'multiple'=>false, 'tl_class'=>'w50', 'hideInput'=>true, 'readonly'=>true),
+    'eval'                    => array('mandatory'=>false, 'multiple'=>false, 'tl_class'=>'clr', 'hideInput'=>true,),
     'options_callback'	      => array('Bcs\Backend\ServiceBackend', 'getPriceTierDisplay'),
     'sql'                     => "TEXT(20000) NOT NULL default ''"
 );
