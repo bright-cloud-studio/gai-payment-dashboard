08280c87adb644fb86b6d9ec00666b19f2b97f6b
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-10 14:23:05 -0400

    Update tl_form_field.php

--- src/Resources/contao/dca/tl_form_field.php
+++ src/Resources/contao/dca/tl_form_field.php
@@ -1,3 +1,5 @@
 <?php
-
-   $GLOBALS['TL_DCA']['tl_form_field']['palettes']['select_dynamic'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,multiple;{options_legend},options;{expert_legend:hide},class,accesskey;{template_legend:hide},customTpl;{invisible_legend:hide},invisible';
+    $GLOBALS['TL_DCA']['tl_form_field']['config']['onload_callback'][] = function() {
+        $GLOBALS['TL_DCA']['tl_form_field']['palettes']['select_dynamic'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,multiple;{options_legend},options;{expert_legend:hide},class,accesskey;{template_legend:hide},customTpl;{invisible_legend:hide},invisible';
+    };
+   
