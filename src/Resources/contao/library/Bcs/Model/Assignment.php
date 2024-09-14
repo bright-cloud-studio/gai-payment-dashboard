588e509d7231f0672e1707a6f4c64812919ba6eb
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-14 19:44:29 -0400

    Update Assignment.php

--- src/Resources/contao/library/Bcs/Model/Assignment.php
+++ src/Resources/contao/library/Bcs/Model/Assignment.php
@@ -13,5 +13,12 @@ class Assignment extends Model
 	 * @var string
 	 */
 	protected static $strTable = 'tl_assignment';
+
+    public static function findAllByShared(array $arrOptions=array())
+	{
+		$t = static::$strTable;
+
+		return static::findBy(array("$t.psychologists_shared!=''"), null, $arrOptions);
+	}
     
 }
