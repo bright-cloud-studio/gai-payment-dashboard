a80900075347bc6f4439473c6b3662d25a3e4ad9
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-16 10:38:52 -0400

    Create tl_member.php

---
+++ src/Resources/contao/languages/en/tl_member.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2024-2025 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-payment-dashboard
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_member']['price_tier_legend']          = 'Price Tier Details';
+$GLOBALS['TL_LANG']['tl_member']['price_tier']                 = array('Price Tier', 'Select the Price Tier this Member belongs to.');
+$GLOBALS['TL_LANG']['tl_member']['price_tier_display']         = array('Service Prices Display', 'Displays the prices for Services within this Price Tier. Used for reference only.');
