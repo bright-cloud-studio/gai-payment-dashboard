0dd0bc8ffdc7ce1a41d82676df1ffba8b3d6a669
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-09-16 10:14:48 -0400

    Create action.save.contact_info_teacher.php

---
+++ src/Resources/public/php/action.save.contact_info_teacher.php
@@ -0,0 +1,28 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Connect to DB
+    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    
+    $myfile = fopen("logs/psy_work_form_save_contact_info_teacher_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
+    
+    // Get data from ajax
+    $assignment_id = $_POST['assignment_id'];
+    $contact_info_teacher = $_POST['contact_info_teacher'];
+    
+    fwrite($myfile, "SAVING: Assignment ID: " . $assignment_id . "\r\n");
+    fwrite($myfile, "SAVING: Contact Info Teacher: " . $contact_info_teacher . "\r\n");
+
+    $update =  "update tl_assignment set contact_info_teacher='".$contact_info_teacher."' WHERE id='".$assignment_id."'";
+    $result_update = $dbh->query($update);
+
+    fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
+    
+    fclose($myfile);
+    echo "success";
