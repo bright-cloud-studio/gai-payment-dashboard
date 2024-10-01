4b6ba527a232e23fafa99c5a872d3f46b12a0d49
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-01 11:17:36 -0400

    Update GenerateInvoices.php

--- src/Cron/GenerateInvoices.php
+++ src/Cron/GenerateInvoices.php
@@ -5,13 +5,20 @@ namespace App\Cron;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
 
 #[AsCronJob('minutely')]
-class InactiveMembershipCron
+class GenerateInvoices
 {
     public function __invoke(): void
     {
+
+        if (null !== $this->contaoCronLogger) {
+            $this->contaoCronLogger->info("GAI Cron Triggered!");
+        }
+        
         $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . "cron_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
         fwrite($myfile, "CRON Triggered! \n");
         fclose($myfile);
+
+        
         
     }
 }
