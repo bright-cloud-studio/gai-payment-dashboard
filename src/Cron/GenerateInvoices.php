dcb31701b35b0bfaca76adf887b955da02790e9d
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-01 11:33:11 -0400

    Update GenerateInvoices.php

--- src/Cron/GenerateInvoices.php
+++ src/Cron/GenerateInvoices.php
@@ -7,21 +7,21 @@ namespace Bcs\PaymentDashboardBundle\Cron;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
 
 
-/** 
- * @CronJob("minutely")
- */
+#[AsCronJob('minutely')]
 class GenerateInvoices
 {
-    public function __invoke()
+    public function __construct(
+        private readonly Connection $connection,
+        private readonly Filesystem $filesystem,
+        private readonly LoggerInterface|null $contaoCronLogger = null,
+    ) {
+    }
+
+    public function __invoke(): void
     {
-
-        $this->contaoCronLogger->info("GAI Cron Triggered!");
-        
-        $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . "cron_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
-        fwrite($myfile, "CRON Triggered! \n");
-        fclose($myfile);
-
-        
+        if (null !== $this->contaoCronLogger) {
+            $this->contaoCronLogger->info("Cron Hit!");
+        }
         
     }
 }
