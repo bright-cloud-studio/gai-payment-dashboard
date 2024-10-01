b9ef3033c83a0fdc884f8d9b28ecffdc0558ab5c
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-01 12:12:02 -0400

    Update GenerateInvoices.php

--- src/Cron/GenerateInvoices.php
+++ src/Cron/GenerateInvoices.php
@@ -1,32 +1,11 @@
 <?php
 
-declare(strict_types=1);
+namespace Bcs\Cron;
 
-
-namespace Bcs\Crons;
-
-use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Filesystem\Path;
-use Symfony\Component\Finder\Finder;
-
-#[AsCronJob('minutely')]
 class GenerateInvoices
 {
-    public function __construct(
-        private readonly Filesystem $filesystem,
-        private readonly string $projectDir,
-        private readonly LoggerInterface|null $logger,
-    ) {
-    }
-
-    public function __invoke(): void
+    public function __invoke()
     {
-        //$finder = Finder::create()->in(Path::join($this->projectDir, 'system/tmp'));
-
-        //$this->filesystem->remove($finder->getIterator());
-
         $this->logger?->info('GAI Cron Hit!');
     }
 }
