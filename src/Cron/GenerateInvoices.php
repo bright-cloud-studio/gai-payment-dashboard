38c286f765b56e75458bd8574265ff2c1cbb309e
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-01 12:59:51 -0400

    Create GenerateInvoices.php

---
+++ src/Cron/GenerateInvoices.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_tabellen.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+namespace Bcs\Cron;
+
+use Contao\CalendarEventsModel;
+use Contao\CalendarModel;
+use Contao\CoreBundle\Cache\EntityCacheTags;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\System;
+
+class GenerateInvoices
+{
+    public function __construct(
+        private ContaoFramework $framework,
+        private EntityCacheTags $entityCacheTags,
+        private H4aEventAutomator $h4aEventAutomator,
+    ) {
+        $this->framework->initialize();
+    }
+
+    public function updateEvents(): void
+    {
+        System::getContainer()
+                ->get('monolog.logger.contao.cron')
+                ->info('GAI WOOO')
+            ;
+    }
+}
