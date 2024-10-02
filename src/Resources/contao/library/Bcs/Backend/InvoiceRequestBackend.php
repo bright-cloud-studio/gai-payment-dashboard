9e0c3a5406254ba31f29d9aadf32ee496826bc31
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-02 12:30:12 -0400

    Update InvoiceRequestBackend.php

--- src/Resources/contao/library/Bcs/Backend/InvoiceRequestBackend.php
+++ src/Resources/contao/library/Bcs/Backend/InvoiceRequestBackend.php
@@ -127,7 +127,7 @@ class InvoiceRequestBackend extends Backend
 
         // Use the DB to grab all of our enabled members, aka our psychologists
 		$this->import('Database');
-		$result = $this->Database->prepare("SELECT * FROM tl_member WHERE disable=0")->execute();
+		$result = $this->Database->prepare("SELECT * FROM tl_member WHERE disable=0 ORDER BY firstname ASC")->execute();
 		while($result->next())
 		{
             // Add ti array with ID as the value and firstname lastname as the label
@@ -145,7 +145,7 @@ class InvoiceRequestBackend extends Backend
 
         // Use the DB to grab all of our enabled members, aka our psychologists
 		$this->import('Database');
-		$result = $this->Database->prepare("SELECT * FROM tl_district WHERE published=1")->execute();
+		$result = $this->Database->prepare("SELECT * FROM tl_district WHERE published=1 ORDER BY district_name ASC")->execute();
 		while($result->next())
 		{
             // Add ti array with ID as the value and firstname lastname as the label
@@ -163,7 +163,7 @@ class InvoiceRequestBackend extends Backend
 
         // Use the DB to grab all of our enabled members, aka our psychologists
 		$this->import('Database');
-		$result = $this->Database->prepare("SELECT * FROM tl_school WHERE published=1")->execute();
+		$result = $this->Database->prepare("SELECT * FROM tl_school WHERE published=1 ORDER BY school_name ASC")->execute();
 		while($result->next())
 		{
             // Add ti array with ID as the value and firstname lastname as the label
@@ -181,7 +181,7 @@ class InvoiceRequestBackend extends Backend
 
         // Use the DB to grab all of our enabled members, aka our psychologists
 		$this->import('Database');
-		$result = $this->Database->prepare("SELECT * FROM tl_service WHERE published=1")->execute();
+		$result = $this->Database->prepare("SELECT * FROM tl_service WHERE published=1 ORDER BY name ASC")->execute();
 		while($result->next())
 		{
             // Add ti array with ID as the value and firstname lastname as the label
