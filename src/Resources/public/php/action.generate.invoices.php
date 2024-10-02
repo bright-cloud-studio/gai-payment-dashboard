91c3437f309194754b3c980857f958f765733c4d
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-02 10:32:51 -0400

    Update action.generate.invoices.php

--- src/Resources/public/php/action.generate.invoices.php
+++ src/Resources/public/php/action.generate.invoices.php
@@ -39,7 +39,93 @@
 	/*******************/
   	
     // Load our HTML template
-    $html = file_get_contents('/../templates/invoice_psy.html', true);
+    $html = file_get_contents('bundles/bcspaymentdashboard/templates/invoice_psy.html', true);
+
+     // Find all instances of our tag brackets '{{tag}}' and store them in the $tags array
+    preg_match_all('/\{{2}(.*?)\}{2}/is', $html, $tags);
+    
+    // Loop through those tags and replace them with the correct product data
+    foreach($tags[0] as $tag) {
+        
+        // Remove brackets from our tag
+        $cleanTag = str_replace("{{","",$tag);
+        $cleanTag = str_replace("}}","",$cleanTag);
+        
+        // Explode our tag into two parts
+	    $explodedTag = explode("::", $cleanTag);
+	    
+	    // Do different things based on the first part of our tag
+	    switch($explodedTag[0]) {
+		    
+		    // If the first part of our exploded tag is "product" we are looking for an attribute
+		    case 'product':
+		        
+		        // Get the product attribute that is based on our second half of the tag
+		        // This thing is super powerful. Trying to reference the class data typically required knowing the name ahead of time
+		        // But now this way by closing our variable in "{ }" brackets it acts as if it's a fixed name, so we can get any product
+		        // data without having to write each tag manually. Just put in a products attribute name in the template and we will get
+		        // back the correct data. Super happy with this, I did not know it was possible.
+		        switch($explodedTag[1]) {
+		            case 'name':
+		                $html = str_replace($tag, $product_data->{$explodedTag[1]}, $html);
+		                break;
+		            case 'description':
+		                $html = str_replace($tag, $product_data->{$explodedTag[1]}, $html);
+		                break;
+		            default:
+		                
+		                $title = ucwords($explodedTag[1]);
+		                $title = str_replace("_"," ",$title);
+		                
+		                $buffer = '';
+		                $buffer .= "<div class='attribute " . $explodedTag[1] . "'>";
+		                $buffer .= "<h3>" . $title . "</h3>";
+		                $buffer .= $product_data->{$explodedTag[1]};
+		                $buffer .= "</div>";
+		                
+		                $html = str_replace($tag, $buffer, $html);
+		                
+		                break;
+		        }
+		        
+		    
+		    break;
+		    
+		    //colors
+		    case 'options':
+		        
+		        $buffer = '';
+	            foreach($product_options as $thing) {
+                    $buffer .= $thing[$explodedTag[1]];
+                }
+                $html = str_replace($tag, $buffer, $html);
+		        
+		        break;
+		        
+		    case 'year':
+		        $buffer = date('Y');
+                $html = str_replace($tag, $buffer, $html);
+		        
+		        break;
+		        
+		    case 'site_url':
+		        $buffer = "https://" . $_SERVER['SERVER_NAME'];
+                $html = str_replace($tag, $buffer, $html);
+		        
+		        break;
+		        
+		  
+	    }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -60,6 +146,7 @@
 	
 	$output = $dompdf->output();
     $file_addr = $_SERVER['DOCUMENT_ROOT'] . '/../files/invoices/';
-    //file_put_contents($file_addr . 'Test_' . time() . '.pdf', $output);
-    file_put_contents($file_addr . 'LOG_' . time() . '.html', $html);
+    file_put_contents($file_addr . 'Test_' . time() . '.pdf', $output);
+    //file_put_contents($file_addr . 'LOG_' . time() . '.html', $html);
+    
 
