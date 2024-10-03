c263fbe375fcdc5e73a789293fc42756731cc145
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-03 14:29:49 -0400

    Update action.generate.invoices.php

--- src/Resources/public/php/action.generate.invoices.php
+++ src/Resources/public/php/action.generate.invoices.php
@@ -13,22 +13,52 @@
     /********************/
 	/* INITALIZE STUFFS */
 	/********************/
+	
+	// Connect to the DB
+    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+	
     // Create our Dompdf Options class
     $options = new Options();
     // Set our options
     $options->set("defaultFont", "Helvetica");
     $options->set("isRemoteEnabled", "true");
-
-
-
-	// Initialize Dompdf using our just set up Options
-	$dompdf = new Dompdf($options);
     
-    // Connect to the DB
-    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
-    if ($dbh->connect_error) {
-        die("Connection failed: " . $dbh->connect_error);
+    // Get all Districts
+    $districts = array();
+    $d_q =  "SELECT * FROM tl_district WHERE published='1'";
+    $d_r = $dbh->query($d_q);
+    if($d_r) {
+        while($r = $d_r->fetch_assoc()) {
+            $districts[$r['id']] = $r['district_name'];
+        }
     }
+    
+    // Get all Schools
+    $schools = array();
+    $s_q =  "SELECT * FROM tl_school WHERE published='1'";
+    $s_r = $dbh->query($s_q);
+    if($s_r) {
+        while($r = $s_r->fetch_assoc()) {
+            $schools[$r['id']] = $r['school_name'];
+        }
+    }
+    
+    // Get all Students
+    $students = array();
+    $st_q =  "SELECT * FROM tl_student WHERE published='1'";
+    $st_r = $dbh->query($st_q);
+    if($st_r) {
+        while($r = $st_r->fetch_assoc()) {
+            $students[$r['id']] = getInitials($r['name']);
+        }
+    }
+
+
+
+    
 
     
     // Step One
@@ -43,7 +73,8 @@
 
             // Step Two
             // Find Invoices without a PDF link
-            $invoice_query =  "SELECT * FROM tl_invoice WHERE pid='$request' AND invoice_url IS NULL ORDER BY id ASC";
+            //$invoice_query =  "SELECT * FROM tl_invoice WHERE pid='$request' AND invoice_url IS NULL ORDER BY id ASC";
+            $invoice_query =  "SELECT * FROM tl_invoice WHERE pid='$request' ORDER BY id ASC";
             $invoice_result = $dbh->query($invoice_query);
             if($invoice_result) {
                 while($db_inv = $invoice_result->fetch_assoc()) {
@@ -56,7 +87,12 @@
                         mkdir($addr_folder, 0777, true);
                     }
                     
-                    generateInvoice($dompdf, $addr_folder, $filename);
+                    $invoice_id = $db_inv['id'];
+                    $psy_id = $db_inv['psychologist'];
+                    $transaction_ids = $db_inv['transaction_ids'];
+                    
+                    $invoice_folder = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/files/invoices/' . cleanName($db_inv['psychologist_name']) . '/';
+                    generateInvoice($dbh, $invoice_id, $psy_id, $invoice_folder, $addr_folder, $filename, $transaction_ids, $districts, $schools, $students);
                     
                     // Step Four
                     // Generate Our Invoice!
@@ -77,12 +113,24 @@
     }
     
     
-    function generateInvoice($dompdf, $addr_folder, $filename) {
+    function generateInvoice($dbh, $invoice_id, $psy_id, $invoice_folder, $addr_folder, $filename, $transaction_ids, $districts, $schools, $students) {
         
+        // Initialize Dompdf using our just set up Options
+	    $dompdf = new Dompdf($options);
         
         /*******************/
     	/* TEMPLATE STUFFS */
     	/*******************/
+    	
+    	$psy_query =  "SELECT * FROM tl_member WHERE id='$psy_id'";
+        $psy_result = $dbh->query($psy_query);
+        $psy = [];
+        if($psy_result) {
+            while($row = $psy_result->fetch_assoc()) {
+                $psy['name'] = $row['firstname'] . " " . $row['lastname'];
+            }
+        }
+    	
       	
         // Load our HTML template
         $html = file_get_contents('bundles/bcspaymentdashboard/templates/invoice_psy.html', true);
@@ -104,33 +152,20 @@
     	    switch($explodedTag[0]) {
     		    
     		    // If the first part of our exploded tag is "product" we are looking for an attribute
-    		    case 'product':
+    		    case 'psychologist':
     		        
-    		        // Get the product attribute that is based on our second half of the tag
-    		        // This thing is super powerful. Trying to reference the class data typically required knowing the name ahead of time
-    		        // But now this way by closing our variable in "{ }" brackets it acts as if it's a fixed name, so we can get any product
-    		        // data without having to write each tag manually. Just put in a products attribute name in the template and we will get
-    		        // back the correct data. Super happy with this, I did not know it was possible.
+
     		        switch($explodedTag[1]) {
     		            case 'name':
-    		                $html = str_replace($tag, $product_data->{$explodedTag[1]}, $html);
+    		                $html = str_replace($tag, $psy['name'], $html);
     		                break;
-    		            case 'description':
-    		                $html = str_replace($tag, $product_data->{$explodedTag[1]}, $html);
+    		            case 'date_issued':
+    		                $html = str_replace($tag, date('m/d/y'), $html);
+    		                break;
+    		            case 'invoice_number':
+    		                $html = str_replace($tag, date('Y_m'), $html);
     		                break;
     		            default:
-    		                
-    		                $title = ucwords($explodedTag[1]);
-    		                $title = str_replace("_"," ",$title);
-    		                
-    		                $buffer = '';
-    		                $buffer .= "<div class='attribute " . $explodedTag[1] . "'>";
-    		                $buffer .= "<h3>" . $title . "</h3>";
-    		                $buffer .= $product_data->{$explodedTag[1]};
-    		                $buffer .= "</div>";
-    		                
-    		                $html = str_replace($tag, $buffer, $html);
-    		                
     		                break;
     		        }
     		        
@@ -138,29 +173,66 @@
     		    break;
     		    
     		    //colors
-    		    case 'options':
+    		    case 'invoice':
     		        
-    		        $buffer = '';
-    	            foreach($product_options as $thing) {
-                        $buffer .= $thing[$explodedTag[1]];
-                    }
-                    $html = str_replace($tag, $buffer, $html);
+    		        switch($explodedTag[1]) {
+    		            case 'transactions':
+    		                
+    		                $transactions = array();
+    		                $transactions_query =  "SELECT * FROM tl_transaction WHERE id IN (". $transaction_ids .") ORDER BY date_submitted ASC";
+                            $transactions_results = $dbh->query($transactions_query);
+                            if($transactions_results) {
+                                $i = 0;
+                                while($row = $transactions_results->fetch_assoc()) {
+                                    
+                                    // Get the Assignment for this Transaction
+                                    $a_district = '';
+                                    $a_school = '';
+                                    $a_student = '';
+                                    $a_q =  "SELECT * FROM tl_assignment WHERE id='".$row['pid']."'";
+                                    $a_r = $dbh->query($a_q);
+                                    if($a_r) {
+                                        while($a = $a_r->fetch_assoc()) {
+                                            $a_district = $a['district'];
+                                            $a_school = $a['school'];
+                                            $a_student = $a['student'];
+                                        }
+                                    }
+                                    
+                                    $transactions[$i]['district'] = $districts[$a_district];
+                                    $transactions[$i]['school'] = $schools[$a_school];
+                                    $transactions[$i]['student'] = $students[$a_student];
+                                    
+                                    $transactions[$i]['number'] = 'L123/S456';
+                                    
+                                    $transactions[$i]['service'] = $row['service'];
+                                    $transactions[$i]['price'] = $row['price'];
+                                    $i++;
+                                }
+                            }
+    		                
+    		                
+    		                $trans_html = '';
+    		                foreach($transactions as $transaction) {
+    		                    $trans_html .= "<tr>";
+        		                $trans_html .= "<td>" . $transaction['district'] . "</td>";
+        		                $trans_html .= "<td>" . $transaction['school'] . "</td>";
+        		                $trans_html .= "<td>" . $transaction['student'] . "</td>";
+        		                $trans_html .= "<td>" . $transaction['number'] . "</td>";
+        		                $trans_html .= "<td>" . $transaction['service'] . "</td>";
+        		                $trans_html .= "<td>" . $transaction['price'] . "</td>";
+        		                $trans_html .= "</tr>";
+    		                }
+    		                
+    		                
+    		                
+    		                $html = str_replace($tag, $trans_html, $html);
+    		                break;
+    		        }
     		        
     		        break;
-    		        
-    		    case 'year':
-    		        $buffer = date('Y');
-                    $html = str_replace($tag, $buffer, $html);
-    		        
-    		        break;
-    		        
-    		    case 'site_url':
-    		        $buffer = "https://" . $_SERVER['SERVER_NAME'];
-                    $html = str_replace($tag, $buffer, $html);
-    		        
-    		        break;
-    		        
-    		  
+
+
     	    }
             
         }
@@ -181,14 +253,20 @@
     	$dompdf->render();
     	
     	$output = $dompdf->output();
+    	
         file_put_contents($addr_folder . '/' . $filename . '.pdf', $output);
-        //file_put_contents($file_addr . 'LOG_' . time() . '.html', $html);
+        
+        $pdf_url = $invoice_folder . $filename . '.pdf';
+        $url_query =  "UPDATE tl_invoice SET invoice_url='$pdf_url' WHERE id='$invoice_id'";
+        $url_result = $dbh->query($url_query);
+
         
         
         
         
     }
 
+    // Generates our "clean name" which is 'first_last' format
     function cleanName($name) {
         // Remove special characters using regular expression
         $name = preg_replace('/[^a-zA-Z0-9 ]/', '', $name);
@@ -200,4 +278,16 @@
         $name = str_replace(' ', '_', $name);
     
         return $name;
+    }
+    
+    // Generates initials from names
+    function getInitials($name) {
+        $words = explode(" ", $name);
+        $initials = "";
+    
+        foreach ($words as $word) {
+            $initials .= strtoupper(substr($word, 0, 1));
+        }
+    
+        return $initials;
     }
