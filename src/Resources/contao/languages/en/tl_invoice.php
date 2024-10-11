cf9765d8deeae0023d75ce1dd8c1aa88c0321f6c
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-11 13:06:41 -0400

    Create tl_invoice.php

---
+++ src/Resources/contao/languages/en/tl_invoice.php
@@ -0,0 +1,22 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_invoice']['new']                        = array('New Invoice', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_invoice']['show']                       = array('Invoice details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice']['edit']                       = array('Edit Invoice', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice']['copy']                       = array('Copy Invoice', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice']['delete']                     = array('Delete Invoice', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice']['toggle']                     = array('Toggle Invoice', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_invoice']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_invoice']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_invoice']['published']                  = array('Published', 'Publishing a District will make it available as an option when creating Assignments.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_invoice']['invoice_legend']             = 'Invoice Information';
+$GLOBALS['TL_LANG']['tl_invoice']['psychologist']               = array('Psychologist', 'The Psychologist this Invoice belongs to');
+$GLOBALS['TL_LANG']['tl_invoice']['psychologist_name']          = array('Psychologist Name', 'Display Name used for internal purposes');
+$GLOBALS['TL_LANG']['tl_invoice']['invoice_url']                = array('Invoice URL', 'URL Address to the PDF of this Invoice');
+$GLOBALS['TL_LANG']['tl_invoice']['transaction_ids']            = array('Transaction IDs', 'IDs for the Transactions stamped on this Invoice');
+$GLOBALS['TL_LANG']['tl_invoice']['misc_transaction_ids']       = array('Misc. Transaction IDs', 'IDs for the Misc. Transactions stamped on this Invoice');
