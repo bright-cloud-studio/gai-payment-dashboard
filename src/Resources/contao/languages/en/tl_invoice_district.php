5d4677a2eaa20a7063c80852c1e06654604b36bb
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-11 13:12:08 -0400

    Create tl_invoice_district.php

---
+++ src/Resources/contao/languages/en/tl_invoice_district.php
@@ -0,0 +1,24 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_invoice_district']['new']                        = array('New Invoice', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_invoice_district']['show']                       = array('Invoice details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_district']['edit']                       = array('Edit Invoice', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_district']['copy']                       = array('Copy Invoice', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_district']['delete']                     = array('Delete Invoice', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_invoice_district']['toggle']                     = array('Toggle Invoice', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_invoice_district']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_invoice_district']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_invoice_district']['published']                  = array('Published', 'Publishing a District will make it available as an option when creating Assignments.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_invoice_district']['invoice_legend']             = 'Invoice Information';
+$GLOBALS['TL_LANG']['tl_invoice_district']['district']                   = array('District', 'The District this Invoice belongs to');
+$GLOBALS['TL_LANG']['tl_invoice_district']['invoice_url']                = array('Invoice URL', 'URL Address to the PDF of this Invoice');
+
+$GLOBALS['TL_LANG']['tl_invoice_district']['internal_legend']            = 'INTERNAL Information';
+$GLOBALS['TL_LANG']['tl_invoice_district']['district_name']              = array('District Name', 'Display Name used for internal purposes');
+$GLOBALS['TL_LANG']['tl_invoice_district']['transaction_ids']            = array('Transaction IDs', 'IDs for the Transactions stamped on this Invoice');
+$GLOBALS['TL_LANG']['tl_invoice_district']['misc_transaction_ids']       = array('Misc. Transaction IDs', 'IDs for the Misc. Transactions stamped on this Invoice');
