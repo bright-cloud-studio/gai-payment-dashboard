8819d5eb982294476a4aac6b9f4092d641ae44d8
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-10-30 14:27:54 -0400

    Update ModAdminReview.php

--- src/Resources/contao/library/Bcs/Module/ModAdminReview.php
+++ src/Resources/contao/library/Bcs/Module/ModAdminReview.php
@@ -12,9 +12,17 @@
 
 namespace Bcs\Module;
 
-use Bcs\Model\Invoice;
+use Bcs\Model\Assignment;
+use Bcs\Model\District;
+use Bcs\Model\PriceTier;
+use Bcs\Model\School;
+use Bcs\Model\Service;
+use Bcs\Model\Student;
+use Bcs\Model\Transaction;
+use Bcs\Model\TransactionMisc;
 
 use Contao\BackendTemplate;
+use Contao\MemberModel;
 use Contao\System;
 use Contao\FrontendUser;
 
@@ -25,13 +33,15 @@ class ModAdminReview extends \Contao\Module
     /* Default Template */
     protected $strTemplate = 'mod_admin_review';
     
-    protected static $template_invoices = array();
+    /* Stores our table data per Psychologist */    
+    protected static $template_psychologists = array();
+    protected static $template_totals = array();
 
     /* Construct function */
     public function __construct($objModule, $strColumn='main')
-    {
+	{
         parent::__construct($objModule, $strColumn);
-    }
+	}
 
     /* Generate function */
     public function generate()
@@ -57,21 +67,88 @@ class ModAdminReview extends \Contao\Module
 
     protected function compile()
     {
-        
-        $member = FrontendUser::getInstance();
             
         // Include Datatables JS library and CSS stylesheets
         $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/bcspaymentdashboard/js/datatables.min.js';
         $GLOBALS['TL_CSS'][]        = 'bundles/bcspaymentdashboard/css/datatables.min.css';
 
-        $invoices = Invoice::findBy(['psychologist = ?'], [$member->id]);
+
+
+
+
+        // Get all active Psychologists
+        $opt = ['order' => 'firstname ASC'];
+        $psychologists = MemberModel::findBy('disable', '0', $opt);
         
-        foreach($invoices as $invoice) {
-            $template_invoices[$invoice->id]['created'] = date("m/d/y", $invoice->tstamp);
-            $template_invoices[$invoice->id]['invoice_url'] = $invoice->invoice_url;
+        // loop through each
+        foreach($psychologists as $psy) {
+            
+            // Get Transactions
+            $transactions = Transaction::findBy(['psychologist = ?'], [$psy->id]);
+            $transactions_total = 0.00;
+        
+            foreach($transactions as $transaction) {
+    
+                $assignment = Assignment::findOneBy('id', $transaction->pid);
+                
+                $template_psychologists[$psy->id][$transaction->id]['id'] = $transaction->id;
+    
+                // Reviewed
+                if($transaction->published == '1')
+                    $template_psychologists[$psy->id][$transaction->id]['reviewed'] = 'Reviewed';
+                else
+                    $template_psychologists[$psy->id][$transaction->id]['reviewed'] = 'Unreviewed';
+                
+                // District
+                $district = District::findOneBy('id', $assignment->district);
+                $template_psychologists[$psy->id][$transaction->id]['district'] = $district->district_name;
+                // School
+                $school = School::findOneBy('id', $assignment->school);
+                $template_psychologists[$psy->id][$transaction->id]['school'] = $school->school_name;
+                // Student
+                $student = Student::findOneBy('id', $assignment->student );
+                $template_psychologists[$psy->id][$transaction->id]['student'] = $student->name;
+                // Lasid
+                $template_psychologists[$psy->id][$transaction->id]['lasid'] = $student->lasid;
+                // Sasid
+                $template_psychologists[$psy->id][$transaction->id]['sasid'] = $student->sasid;
+                
+                // Service
+                $service = Service::findOneBy('service_code', $transaction->service);
+                if($transaction->meeting_duration > 0) {
+                    $template_psychologists[$psy->id][$transaction->id]['service'] = $service->name . " (" . $transaction->meeting_duration . " minutes)";
+                } else {
+                    $template_psychologists[$psy->id][$transaction->id]['service'] = $service->name;
+                }
+                
+                
+                // Price
+                if($transaction->price != '') {
+                    if($service->service_code == 1) {
+                        
+                        $dur = ceil(intval($transaction->meeting_duration) / 60);
+                        $final_price = $dur * $transaction->price;
+                        
+                        $template_psychologists[$psy->id][$transaction->id]['price'] = number_format(floatval($final_price), 2, '.', '');
+                        $transactions_total += number_format(floatval($final_price), 2, '.', '');
+                    } else if($service->service_code == 19) {
+                        $final_price = $transaction->meeting_duration * 0.50;
+                        $template_psychologists[$psy->id][$transaction->id]['price'] = number_format(floatval($final_price), 2, '.', '');
+                        $transactions_total += number_format(floatval($final_price), 2, '.', '');
+                    } else {
+                        $template_psychologists[$psy->id][$transaction->id]['price'] = number_format(floatval($transaction->price), 2, '.', '');
+                        $transactions_total += number_format(floatval($transaction->price), 2, '.', '');
+                    }
+                }
+                
+                
+            }
+            $template_totals[$psy->id] = $transactions_total;
+            
         }
-        
-        $this->Template->invoices = $template_invoices;
+
+        $this->Template->totals = $template_totals;
+        $this->Template->psychologists = $template_psychologists;
         
     }
   
