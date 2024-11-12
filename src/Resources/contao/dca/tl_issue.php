d69e5b9c3d586592d32f963808c99cb1ab8421f2
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-11-12 15:11:39 -0500

    Create tl_issue.php

---
+++ src/Resources/contao/dca/tl_issue.php
@@ -0,0 +1,181 @@
+<?php
+
+use Contao\Backend;
+use Contao\Database;
+use Contao\DataContainer;
+use Contao\DC_Table;
+use Contao\Input;
+ 
+/* Table tl_services */
+$GLOBALS['TL_DCA']['tl_issue'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => DC_Table::class,
+        'switchToEdit'                => false,
+        'onload_callback' => array
+    		(
+    			  array('tl_issue', 'setRootType')
+    		),
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => DataContainer::MODE_SORTED,
+            'rootPaste'               => false,
+            'showRootTrails'          => false,
+            'icon'                    => 'pagemounts.svg',
+            'flag'                    => DataContainer::SORT_INITIAL_LETTERS_ASC,
+            'fields'                  => array('status'),
+            'panelLayout'             => 'sort,filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('status', 'name'),
+			      'format'                  => '%s - %s',
+			      'label_callback'          => array('tl_issue', 'addIcon')
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_issue']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+            'toggle' => array
+      			(
+      				'label'               => &$GLOBALS['TL_LANG']['tl_issue']['toggle'],
+      				'icon'                => 'visible.gif',
+      				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+      				'button_callback'     => array('Bcs\Backend\DistrictBackend', 'toggleIcon')
+      			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_issue']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{issue_legend}, status, title, description;{publish_legend}, published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+            'sql'                   => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+            'sql'                   => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'sorting' => array
+        (
+            'sql'                   => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'status' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_issue']['status'],
+            'inputType'               => 'select',
+            'default'                 => 'outstanding',
+            'filter'                  => true,
+            'search'                  => true,
+            'options'                  => array('outstanding' => 'Outstanding', 'resolved' => 'Resolved'),
+    		    'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50'),
+    		    'sql'                      => "varchar(15) NOT NULL default ''"
+        ),
+        'title' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_issue']['title'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'filter'                  => true,
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>false, 'tl_class'=>'clr', 'rte'=>'tinyMCE'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'description' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_issue']['description'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'filter'                  => true,
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>false, 'tl_class'=>'clr', 'rte'=>'tinyMCE'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+
+
+        'published' => array
+        (
+            'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['published'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('submitOnChange'=>false, 'doNotCopy'=>true),
+            'sql'                     => "char(1) NOT NULL default ''"
+        )
+    )
+);
+
+
+
+class tl_issue extends Backend
+{
+	public function setRootType(DataContainer $dc)
+	{
+		if (Input::get('act') != 'create')
+		{
+			return;
+		}
+		if (Input::get('pid') == 0)
+		{
+			$GLOBALS['TL_DCA']['tl_service']['fields']['type']['default'] = 'root';
+		}
+		elseif (Input::get('mode') == 1)
+		{
+			$objPage = Database::getInstance()
+				->prepare("SELECT * FROM " . $dc->table . " WHERE id=?")
+				->limit(1)
+				->execute(Input::get('pid'));
+
+			if ($objPage->pid == 0)
+			{
+				$GLOBALS['TL_DCA']['tl_issue']['fields']['type']['default'] = 'root';
+			}
+		}
+	}
+
+    public function addIcon($row, $label, DataContainer|null $dc=null, $imageAttribute='', $blnReturnImage=false, $blnProtected=false, $isVisibleRootTrailPage=false)
+	{
+		return Backend::addPageIcon($row, $label, $dc, $imageAttribute, $blnReturnImage, $blnProtected, $isVisibleRootTrailPage);
+	}
+}
