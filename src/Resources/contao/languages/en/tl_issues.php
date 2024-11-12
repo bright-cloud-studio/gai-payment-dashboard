1b09f82e05b0ee89368f62e5040ebccb6d17e5f9
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-11-12 15:38:17 -0500

    Create tl_issues.php

---
+++ src/Resources/contao/languages/en/tl_issues.php
@@ -0,0 +1,21 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_issue']['new']                        = array('New School', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_issue']['show']                       = array('School Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_issue']['edit']                       = array('Edit School', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_issue']['copy']                       = array('Copy School', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_issue']['delete']                     = array('Delete School', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_issue']['toggle']                     = array('Toggle School', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_issue']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_issue']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_issue']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_issue']['issue_legend']       = 'Issue Information';
+$GLOBALS['TL_LANG']['tl_issue']['status']             = array('Status', 'Select the Status of this Issue, by default all Issues start as Outstanding');
+$GLOBALS['TL_LANG']['tl_issue']['title']              = array('Title', 'Enter a short, but to the point, title for easy reading');
+$GLOBALS['TL_LANG']['tl_issue']['description']        = array('Description', 'Enter as many details as you can regarding this Issue');
+$GLOBALS['TL_LANG']['tl_issue']['publish']            = array('Active', 'Issues marked Active will be worked on')
