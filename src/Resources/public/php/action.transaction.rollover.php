3273d66128c6198be79326b62130e91f6afac596
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-11 13:45:25 -0500

    Update action.transaction.rollover.php

--- src/Resources/public/php/action.transaction.rollover.php
+++ src/Resources/public/php/action.transaction.rollover.php
@@ -11,28 +11,37 @@
     }
 
     // Get the current day and hour
+    $year = date('y');
+    $month = date('n');
     $day = date('j');
     $hour = date("H");
-
 
     // If this is the first day of the month
     if($day == 1) {
         
         // If this is the fifth hour of the day
         if($hour == 5) {
-
             
-            $query = "select * from tl_member WHERE disable='0' AND email!=''";
+            $query = "select * from tl_transaction WHERE published=''";
             $result = $dbh->query($query);
             if($result) {
+
+                $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/../logs/transaction_rollover_' . date('m-d-Y_hia') . '.txt', 'w') or die("Unable to open file!");
+                
                 while($row = $result->fetch_assoc()) {
-                    $row['lastname'];
+
+                    write($myfile, "YEAR: " . $year . " MONTH: " . $month . " DAY: " . $day . " HOUR: " . $hour . "\r\n");
+
+                    $t_year = date_format($row['date_submitted'], 'y');
+                    $t_month = date_format($row['date_submitted'], 'n');
+
+                    write($myfile, "TYEAR: " . $t_year . " TMONTH: " . "\r\n");
+                    
                 }
+
+                fclose($myfile);
+                
             }
 
-
-            
-
-        }
-        
+        }        
     }
