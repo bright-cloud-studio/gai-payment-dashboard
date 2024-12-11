739a5df3d641027e3d1b1626b256a8f731adee3c
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-11 13:03:16 -0500

    Update action.transaction.rollover.php

--- src/Resources/public/php/action.transaction.rollover.php
+++ src/Resources/public/php/action.transaction.rollover.php
@@ -29,51 +29,10 @@
             $result = $dbh->query($query);
             if($result) {
                 while($row = $result->fetch_assoc()) {
-                    
-                    // Send "Final Reminder" Email
-        			$addr = $row['email'];
-        			$headers = "MIME-Version: 1.0" . "\r\n";
-        			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
-        			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
-        			$sub = "The end of the month is here, make sure to Submit and Review your Transactions";
-        			$message = "
-        				<html>
-        				<head>
-        				<title>Global Assessments, Inc. - FINAL DAY Reminder</title>
-        				</head>
-        				<body>
-        					<p>".$row['firstname']." ".$row['lastname'].",</p>
-        					<p>We are a few days away from the end of the month. Please make sure to Submit and Review your Transactions for this month. All reports must be Submitted to Anna by Midnight 24-hours prior to the end of the last business day of the month. All Invoices must be Reviewed and Submitted in the Portal by Midnight on the last day of the month.</p>
-        					<p>Best,<br>Global Assessments, Inc.</p>
-        				</body>
-        				</html>
-        				";
-        			mail($addr, $sub, $message, $headers);
+                    $row['lastname'];
                 }
             }
 
         }
         
     }
-    
-    
-    // Send the "FINAL DAY" email
-    else if($how_many_days == $final_reminder) {
-        
-        // If this is the desired hour
-        if($hour == 12) {
-            
-            $query = "select * from tl_member WHERE disable='0' AND email!=''";
-            $result = $dbh->query($query);
-            if($result) {
-                while($row = $result->fetch_assoc()) {
-                  $row['lastname']
-                  
-                }
-            }
-
-        }
-        
-    }
-
