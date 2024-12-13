87928636f217a673abedc80effb93705c7fc662a
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-13 13:52:51 -0500

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -17,7 +17,7 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['price_tier'] = array
     'label'                   => &$GLOBALS['TL_LANG']['tl_member']['price_tier'],
     'inputType'               => 'select',
     'flag'                    => DataContainer::SORT_ASC,
-    'eval'                    => array('mandatory'=>true, 'multiple'=>false, 'tl_class'=>''),
+    'eval'                    => array('mandatory'=>true, 'multiple'=>false, 'tl_class'=>'', 'submitOnChange'=>false),
     'options_callback'	      => array('Bcs\Backend\ServiceBackend', 'getPriceTiers'),
     'sql'                     => "varchar(50) NOT NULL default ''",
     'default'                 => "tier_1_price"
