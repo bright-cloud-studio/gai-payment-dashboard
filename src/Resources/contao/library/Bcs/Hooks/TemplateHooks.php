747d1c6af3630d1c07f08c0cefeffa6b7eccd73d
Bright Cloud Studio <mark@brightcloudstudio.com>
2024-12-20 13:25:57 -0500

    Create TemplateHooks.php

---
+++ src/Resources/contao/library/Bcs/Hooks/TemplateHooks.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bcs\Hooks;
+
+use Bcs\Model\Service;
+use Bcs\Model\Transaction;
+use Bcs\Model\TransactionMisc;
+
+use Contao\Controller;
+use Contao\Environment;
+use Contao\FrontendUser;
+use Contao\Input;
+use Contao\MemberModel;
+use Contao\PageModel;
+use Contao\Template;
+
+class TemplateHooks
+{
+    protected static $arrUserOptions = array();
+
+    // When a form is submitted
+    public function onParseTemplate($template)
+    {
+      
+    }
+  
+}
