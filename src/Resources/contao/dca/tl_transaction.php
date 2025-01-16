4f5548fcdf2e3a7a1eb62fb685849e3d89c5b347
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-16 12:32:36 -0500

    Update tl_transaction.php

--- src/Resources/contao/dca/tl_transaction.php
+++ src/Resources/contao/dca/tl_transaction.php
@@ -302,7 +302,7 @@ $GLOBALS['TL_DCA']['tl_transaction'] = array
             'filter'                  => false,
             'search'                  => false,
             'eval'                    => array('rgxp'=>'date', 'datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "int(10) unsigned",
+            'sql'                     => "varchar(20) default ''",
             'default'                 => ''
         ),
         
