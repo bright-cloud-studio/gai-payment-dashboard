fd0ac223516b8c6e68c206831c6201a2da4e815b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-31 15:31:12 -0500

    Update action.ar.save.school.php

--- src/Resources/public/php/action.ar.save.school.php
+++ src/Resources/public/php/action.ar.save.school.php
@@ -10,7 +10,7 @@
         die("Connection failed: " . $dbh->connect_error);
     }
     
-    $myfile = fopen("logs/admin_review_save_school_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
+    $myfile = fopen("logs/admin_review_save_school_log_".date('m_d_Y_H:i:s').".txt", "w") or die("Unable to open file!");
     
     // Get data from ajax
     $assignment_id = $_POST['assignment_id'];
