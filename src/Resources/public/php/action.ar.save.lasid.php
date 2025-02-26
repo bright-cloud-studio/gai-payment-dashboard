368168e5aa1b4387d34e43a15ac77b8bc94183cd
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-26 11:57:06 -0500

    Update action.ar.save.lasid.php

--- src/Resources/public/php/action.ar.save.lasid.php
+++ src/Resources/public/php/action.ar.save.lasid.php
@@ -13,13 +13,13 @@
     $myfile = fopen("logs/admin_review_save_lasid_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
     
     // Get data from ajax
-    $assignment_id = $_POST['assignment_id'];
+    $student_id = $_POST['student_id'];
     $lasid = $_POST['lasid'];
     
-    fwrite($myfile, "SAVING: Assignment ID: " . $assignment_id . "\r\n");
+    fwrite($myfile, "SAVING: Student ID: " . $student_id . "\r\n");
     fwrite($myfile, "SAVING: LASID: " . $lasid . "\r\n");
 
-    $update =  "update tl_assignment set lasid='".$lasid."' WHERE id='".$assignment_id."'";
+    $update =  "update tl_student set lasid='".$lasid."' WHERE id='".$student_id."'";
     $result_update = $dbh->query($update);
 
     fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
