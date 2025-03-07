d97b329c0884284eebf90d4e6012627ce3534437
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-07 10:42:41 -0500

    Create action.ar.mark.reviewed.php

---
+++ src/Resources/public/php/action.ar.mark.reviewed.php
@@ -0,0 +1,28 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Connect to DB
+    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    
+    $myfile = fopen("logs/admin_review_mark_reviewed_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
+    
+    // Get data from ajax
+    $psychologist = $_POST['psychologist'];
+    
+    fwrite($myfile, "SAVING: Psychologist ID: " . $psychologist . "\r\n");
+    
+    $date = date("m/d/y");
+
+    $update =  "update tl_member set last_reviewed='".$date."' WHERE id='".$psychologist."'";
+    $result_update = $dbh->query($update);
+
+    fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
+    
+    fclose($myfile);
+    echo "success";
