95732c3761d765e670d2ffc9c72d1a6270d09029
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-18 14:45:36 -0400

    Create action.review.set.last.reviewed.php

---
+++ src/Resources/public/php/action.review.set.last.reviewed.php
@@ -0,0 +1,20 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Connect to DB
+    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+
+    $psy_id = $_POST['psy_id'];
+    
+    $update =  "update tl_member set last_review_and_submit='".time()."' WHERE id='".$psy_id."'";
+    $result_update = $dbh->query($update);
+    
+
+    echo "pass";
+
