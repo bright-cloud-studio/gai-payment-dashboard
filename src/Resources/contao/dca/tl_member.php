4a77b2cf03d8ea7b096f3aafa4e2d7e81bf6013f
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-18 13:32:50 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -8,7 +8,7 @@ use Contao\DC_Table;
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend}, price_tier, price_tier_display;{admin_review_legend}, last_reviewed;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend}, price_tier, price_tier_display;{admin_review_legend}, last_reviewed; {last_review_and_submit_legend}, last_review_and_submit;', $v);
 }
     
 /* Add fields to tl_user */
@@ -45,6 +45,18 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['psych_work_form_colors'] = array
 $GLOBALS['TL_DCA']['tl_member']['fields']['last_reviewed'] = array
 (
     'label'                   => &$GLOBALS['TL_LANG']['tl_member']['psych_work_form_colors'],
+    'inputType'               => 'text',
+    'default'                 => '',
+    'filter'                  => true,
+    'search'                  => true,
+    'eval'                    => array('rgxp'=>'date', 'datepicker'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(20) NOT NULL default ''",
+    'default'                => date('m/d/y'),
+);
+
+$GLOBALS['TL_DCA']['tl_member']['fields']['last_review_and_submit'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['last_review_and_submit'],
     'inputType'               => 'text',
     'default'                 => '',
     'filter'                  => true,
