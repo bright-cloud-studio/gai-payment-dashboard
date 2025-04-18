e369a584b4581dc4767a6530d9a6fc5af9389abc
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-18 15:23:39 -0400

    Update tl_member.php

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -14,3 +14,9 @@
 $GLOBALS['TL_LANG']['tl_member']['price_tier_legend']          = 'Price Tier Details';
 $GLOBALS['TL_LANG']['tl_member']['price_tier']                 = array('Price Tier', 'Select the Price Tier this Member belongs to.');
 $GLOBALS['TL_LANG']['tl_member']['price_tier_display']         = array('DISPLAY ONLY - Service Tier Prices', 'Displays the prices for Services within this Price Tier. Used for reference only.');
+
+$GLOBALS['TL_LANG']['tl_member']['admin_review_legend']        = 'Admin Review Details';
+$GLOBALS['TL_LANG']['tl_member']['last_reviewed']              = array('Last Reviewed Date', 'The date the last time an Admin Review was performed on this Psychologist');
+
+$GLOBALS['TL_LANG']['tl_member']['last_review_and_submit_legend']    = 'Last Reviewed and Submitted Details';
+$GLOBALS['TL_LANG']['tl_member']['last_review_and_submit']           = array('Last Review and Submit', 'The date the last time this Psychologist used the Review Transaction feature');
