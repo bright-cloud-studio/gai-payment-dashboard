6ffbdfe5d300b757d287b884c03399c915af0efd
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-28 13:19:30 -0400

    Update action.review.transaction.php

--- src/Resources/public/php/action.review.transaction.php
+++ src/Resources/public/php/action.review.transaction.php
@@ -10,20 +10,21 @@
         die("Connection failed: " . $dbh->connect_error);
     }
 
-    
-
+    // Get our passed in Transaction ID
     $transaction_id = $_POST['transaction_id'];
     
+    // Determine if Misc. Transaction or not
     if (str_contains($transaction_id, 'm_')) {
         $clean_id = $onlyconsonants = str_replace("m_", "", $transaction_id);
+        
+        // Set this Misc. Transaction as Published and Reviewed
         $update =  "update tl_transaction_misc set published='1' WHERE id='".$clean_id."'";
         $result_update = $dbh->query($update);
     
     } else {
+        // Set this Transaction as Published and Reviewed
         $update =  "update tl_transaction set published='1' WHERE id='".$transaction_id."'";
         $result_update = $dbh->query($update);
     }
-    
 
     echo "fail";
-
