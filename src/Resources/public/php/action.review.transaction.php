27b9d6cbf09b30ae82bdc8069050aa6614c3ff2a
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-29 11:48:18 -0400

    Update action.review.transaction.php

--- src/Resources/public/php/action.review.transaction.php
+++ src/Resources/public/php/action.review.transaction.php
@@ -13,46 +13,50 @@
     // Create log file for debugging purposes
     $myfile = fopen("logs/review_transaction_".date('m_d_Y_H:i:s').".txt", "w") or die("Unable to open file!");
 
-    // Get our passed in Transaction ID
-    $transaction_id = $_POST['transaction_id'];
-    $status = $_POST['status'];
-    
-    // Save our POST values to confirm they came in successfully
-    fwrite($myfile, "SAVING: Transaction ID: " . $transaction_id . "\r\n");
-    fwrite($myfile, "SAVING: Status: " . $status . "\r\n");
-    
-    // Determine if Misc. Transaction or not
-    if (str_contains($transaction_id, 'm_')) {
-        
-        $clean_id = str_replace("m_", "", $transaction_id);
-        
-        // Only update 'created' status, we don't want the status to go backwards
-        if($status == 'created') {
-            // Set this Misc. Transaction as Published and Reviewed
-            $update =  "update tl_transaction_misc set published='1', status='reviewed' WHERE id='".$clean_id."'";
-            $result_update = $dbh->query($update);
-        } else {
-            // Set this Misc. Transaction as Published and Reviewed
-            $update =  "update tl_transaction_misc set published='1' WHERE id='".$clean_id."'";
-            $result_update = $dbh->query($update);
-        }
 
-    } else {
+    if(isset($_POST['transaction_id']) && isset($_POST['status'])) {
+
+        // Get our passed in Transaction ID
+        $transaction_id = $_POST['transaction_id'];
+        $status = $_POST['status'];
         
-        // Only update 'created' status, we don't want the status to go backwards
-        if($status == 'created') {
-            // Set this Transaction as Published and Reviewed
-            $update =  "update tl_transaction set published='1', status='reviewed' WHERE id='".$transaction_id."'";
-            $result_update = $dbh->query($update);
+        // Save our POST values to confirm they came in successfully
+        fwrite($myfile, "SAVING: Transaction ID: " . $transaction_id . "\r\n");
+        fwrite($myfile, "SAVING: Status: " . $status . "\r\n");
+        
+        // Determine if Misc. Transaction or not
+        if (str_contains($transaction_id, 'm_')) {
+            
+            $clean_id = str_replace("m_", "", $transaction_id);
+            
+            // Only update 'created' status, we don't want the status to go backwards
+            if($status == 'created') {
+                // Set this Misc. Transaction as Published and Reviewed
+                $update =  "update tl_transaction_misc set published='1', status='reviewed' WHERE id='".$clean_id."'";
+                $result_update = $dbh->query($update);
+            } else {
+                // Set this Misc. Transaction as Published and Reviewed
+                $update =  "update tl_transaction_misc set published='1' WHERE id='".$clean_id."'";
+                $result_update = $dbh->query($update);
+            }
+    
         } else {
-            // Set this Transaction as Published and Reviewed
-            $update =  "update tl_transaction set published='1' WHERE id='".$transaction_id."'";
-            $result_update = $dbh->query($update);
+            
+            // Only update 'created' status, we don't want the status to go backwards
+            if($status == 'created') {
+                // Set this Transaction as Published and Reviewed
+                $update =  "update tl_transaction set published='1', status='reviewed' WHERE id='".$transaction_id."'";
+                $result_update = $dbh->query($update);
+            } else {
+                // Set this Transaction as Published and Reviewed
+                $update =  "update tl_transaction set published='1' WHERE id='".$transaction_id."'";
+                $result_update = $dbh->query($update);
+            }
+            
         }
-        
+    
+        // Close our log file
+        fclose($myfile);
+    
+        echo "fail";
     }
-
-    // Close our log file
-    fclose($myfile);
-
-    echo "fail";
