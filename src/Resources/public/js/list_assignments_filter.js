e61ee85a66abd514afef88143e40b1be6cdf5adf
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-29 10:49:10 -0400

    Update list_assignments_filter.js

--- src/Resources/public/js/list_assignments_filter.js
+++ src/Resources/public/js/list_assignments_filter.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
   
   // loop through listed elements
   $( ".list_assignment_wrapper" ).each(function( index ) {
-  
+      
     // HOW FILTERING WORKS BY LOGIC
     // 1. Start off with the assumption you will show the entry
     // 2. Check if we should be filtering, and if so, if the entry matches the criteria we are filtering for
@@ -34,16 +34,19 @@ $(document).ready(function() {
     var show = true;
     
     // if filtering by district AND the district data doesnt match our selection then flag to hide
-    if(district != 0 && $(this).data('district') != district)
+    if(district != null && $(this).data('district') != district) {
       show = false;
+    }
     
     // if filtering by school AND the school data doesnt match our selection then flag to hide
-    if(school != 0 && $(this).data('school') != school)
+    if(school != null && $(this).data('school') != school) {
       show = false;
+    }
     
     // if filtering by student AND the student data doesnt match our selection then flag to hide
-    if(student != 0 && $(this).data('student') != student)
+    if(student != null && $(this).data('student') != student) {
       show = false;
+    }
     
     // show if show, hide if not
     if(show)
