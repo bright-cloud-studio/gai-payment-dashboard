bf003d649fdfec33b94d21cf39baf4af0e99fea7
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-06-06 09:06:43 -0400

    Update action.generate.invoices.district.php

--- src/Resources/public/php/action.generate.invoices.district.php
+++ src/Resources/public/php/action.generate.invoices.district.php
@@ -53,7 +53,7 @@
     $st_r = $dbh->query($st_q);
     if($st_r) {
         while($r = $st_r->fetch_assoc()) {
-            $students[$r['id']]['name'] = getInitialsDistrictInvoice($r['name']);
+            $students[$r['id']]['name'] = getInitialsDistrict($r['name']);
             
             if($r['lasid'] != '')
                 $students[$r['id']]['number'] = $r['lasid'];
@@ -519,7 +519,7 @@
     }
     
     // Generates initials from names
-    function getInitialsDistrictInvoice($name) {
+    function getInitialsDistrict($name) {
         $words = explode(" ", $name);
         $initials = "";
     
