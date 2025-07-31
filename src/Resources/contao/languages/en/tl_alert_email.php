9ea29d65cccb1490bfd628c391bc0ab02f627ecb
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-31 13:09:26 -0400

    Create tl_alert_email.php

---
+++ src/Resources/contao/languages/en/tl_alert_email.php
@@ -0,0 +1,18 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_alert_email']['new']                        = array('New Assignment', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_alert_email']['show']                       = array('Assignment Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['edit']                       = array('Edit Assignment', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['copy']                       = array('Copy Assignment', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['delete']                     = array('Delete Assignment', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['toggle']                     = array('Toggle Assignment', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_alert_email']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_alert_email']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_alert_email']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_alert_email']['alert_email_legend']         = 'Alert Email Details';
+$GLOBALS['TL_LANG']['tl_assignment']['month']              = array('Month', 'Enter in a Month label to identify which timeframe this covers');
