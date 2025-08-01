8a2d087c282b5c1750d277d49e00adc6a280cdee
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-01 08:51:17 -0400

    Update tl_alert_email.php

--- src/Resources/contao/languages/en/tl_alert_email.php
+++ src/Resources/contao/languages/en/tl_alert_email.php
@@ -1,18 +1,24 @@
 <?php
 
-/* System Buttons */
-$GLOBALS['TL_LANG']['tl_alert_email']['new']                        = array('New Assignment', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_alert_email']['show']                       = array('Assignment Details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_alert_email']['edit']                       = array('Edit Assignment', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_alert_email']['copy']                       = array('Copy Assignment', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_alert_email']['delete']                     = array('Delete Assignment', 'Delete record ID %s');
-$GLOBALS['TL_LANG']['tl_alert_email']['toggle']                     = array('Toggle Assignment', 'Toggle record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['new']                        = array('New Alert Email', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_alert_email']['show']                       = array('Alert Email Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['edit']                       = array('Edit Alert Email', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['copy']                       = array('Copy Alert Email', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['delete']                     = array('Delete Alert Email', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_alert_email']['toggle']                     = array('Toggle Alert Email', 'Toggle record ID %s');
 
-/* System Fields */
 $GLOBALS['TL_LANG']['tl_alert_email']['publish_legend']             = 'Publish';
-$GLOBALS['TL_LANG']['tl_alert_email']['alias']                      = array('Alias', 'Auto-generated alias.');
 $GLOBALS['TL_LANG']['tl_alert_email']['published']                  = array('Published', 'Show this record on the front end.');
 
-/* Fields */
 $GLOBALS['TL_LANG']['tl_alert_email']['alert_email_legend']         = 'Alert Email Details';
-$GLOBALS['TL_LANG']['tl_assignment']['month']              = array('Month', 'Enter in a Month label to identify which timeframe this covers');
+$GLOBALS['TL_LANG']['tl_alert_email']['month']                      = array('Month', 'Enter in a Month label to identify which timeframe this covers');
+
+$GLOBALS['TL_LANG']['tl_alert_email']['warning_legend']         = 'Week Remaining Details';
+$GLOBALS['TL_LANG']['tl_alert_email']['warning_date']           = array('Send Date', 'Enter in a Month label to identify which timeframe this covers');
+$GLOBALS['TL_LANG']['tl_alert_email']['warning_subject']        = array('Subject', 'Enter in a Month label to identify which timeframe this covers');
+$GLOBALS['TL_LANG']['tl_alert_email']['warning_body']           = array('Body', 'Enter in a Month label to identify which timeframe this covers');
+
+$GLOBALS['TL_LANG']['tl_alert_email']['final_legend']         = 'Final Day Details';
+$GLOBALS['TL_LANG']['tl_alert_email']['final_date']           = array('Send Date', 'Enter in a Month label to identify which timeframe this covers');
+$GLOBALS['TL_LANG']['tl_alert_email']['final_subject']        = array('Subject', 'Enter in a Month label to identify which timeframe this covers');
+$GLOBALS['TL_LANG']['tl_alert_email']['final_body']           = array('Body', 'Enter in a Month label to identify which timeframe this covers');
