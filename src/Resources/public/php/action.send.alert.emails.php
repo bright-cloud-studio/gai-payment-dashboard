50f57dc75a62b03f86d5b5c35f5fb060da5589a6
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-04 12:25:52 -0400

    Update action.send.alert.emails.php

--- src/Resources/public/php/action.send.alert.emails.php
+++ src/Resources/public/php/action.send.alert.emails.php
@@ -13,109 +13,20 @@
     // Create a log file so we can track this is working accurately
     $log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../logs/'.date('m_d_y').'_reminder_emails.txt', "a+") or die("Unable to open file!");
 
-    // Trigger when X days before the end of the month
-    
-    $weekly_reminder = 7;
-    
-    $final_reminder = 0;
-    // Calculates remaining days until the next month
-    $how_many_days = date('t') - date('j');
-    // Gets the current Hour in 24 hour format
-    $hour = date("H");
 
 
-    fwrite($log, "How Many Days: " . $how_many_days . "\r\n");
-    fwrite($log, "Weekly Reminder: " . $weekly_reminder . "\r\n");
-    fwrite($log, "Final Reminder: " . $final_reminder . "\r\n");
-    fwrite($log, "Hour: " . $hour . "\r\n");
 
-    // Send the "Week Remaining" email
-    if($how_many_days == $weekly_reminder) {
-        
-        fwrite($log, "Today is Week Reminder day! \r\n");
-        
-        // If this is the desired hour
-        if($hour == 12) {
-            
-            fwrite($log, "This is the NOON hour! \r\n");
-            
-            $query = "select * from tl_member WHERE disable='0' AND email!=''";
-            $result = $dbh->query($query);
-            if($result) {
-                while($row = $result->fetch_assoc()) {
-                    
-                    // Send "Final Reminder" Email
-        			$addr = $row['email'];
-        			$headers = "MIME-Version: 1.0" . "\r\n";
-        			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
-        			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
-        			
-        			
-        			$sub = "The end of the month is here, make sure to Submit and Review your Transactions";
-        			$message = "
-        				<html>
-        				<head>
-        				<title>Global Assessments, Inc. - FINAL DAY Reminder</title>
-        				</head>
-        				<body>
-        					<p>".$row['firstname']." ".$row['lastname'].",</p>
-        					<p>We are a few days away from the end of the month. Please make sure to Submit and Review your Transactions for this month. All reports must be Submitted to Anna by Midnight 24-hours prior to the end of the last business day of the month. All Invoices must be Reviewed and Submitted in the Portal by Midnight on the last day of the month.</p>
-        					<p>Best,<br>Global Assessments, Inc.</p>
-        				</body>
-        				</html>
-        				";
-        			
-        			
-        			mail($addr, $sub, $message, $headers);
-                }
-            }
 
-        }
-        
-    }
-    
-    
-    
-    // Send the "FINAL DAY" email
-    else if($how_many_days == $final_reminder) {
-        
-        fwrite($log, "Today is FINAL DAY day! \r\n");
-        
-        // If this is the desired hour
-        if($hour == 12) {
-            
-            $query = "select * from tl_member WHERE disable='0' AND email!=''";
-            $result = $dbh->query($query);
-            if($result) {
-                while($row = $result->fetch_assoc()) {
-                    
-                    // Send "Final Reminder" Email
-        			$addr = $row['email'];
-        			$headers = "MIME-Version: 1.0" . "\r\n";
-        			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
-        			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
-        			$sub = "Today is the FINAL DAY to Submit and Review Transactions for this month";
-        			$message = "
-        				<html>
-        				<head>
-        				<title>Global Assessments, Inc. - FINAL DAY Reminder</title>
-        				</head>
-        				<body>
-        					<p>".$row['firstname']." ".$row['lastname'].",</p>
-        					<p>This is a final reminder that all entries MUST be Submitted then Reviewed in the Portal by Midnight tonight in order to process.</p>
-        					<p>Best,<br>Global Assessments, Inc.</p>
-        				</body>
-        				</html>
-        				";
-        			mail($addr, $sub, $message, $headers);
-                }
-            }
 
-        }
-        
-    }
+    // Loop through all Alert emails
+
+        // If it is "SEND DATE", and it is within the noon hour, send the email
+
+            // mark 'last_sent' as today to prevent duplicate mailings
+
+
+
+    
 
     // Close our log file
     fclose($log);
