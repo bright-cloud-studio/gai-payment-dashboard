16e5ad3ca64c4038ff7f69d6d751c89f978fb972
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-20 15:12:12 -0400

    Create action.save.meeting_time.php

---
+++ src/Resources/public/php/action.save.meeting_time.php
@@ -0,0 +1,28 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Connect to DB
+    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    
+    $myfile = fopen("logs/psy_work_form_save_meeting_time_log_".date('m-d-Y_hia').".txt", "w") or die("Unable to open file!");
+    
+    // Get data from ajax
+    $assignment_id = $_POST['assignment_id'];
+    $meeting_time = $_POST['meeting_time'];
+    
+    fwrite($myfile, "SAVING: Assignment ID: " . $assignment_id . "\r\n");
+    fwrite($myfile, "SAVING: Meeting Time: " . $meeting_time . "\r\n");
+
+    $update =  "update tl_assignment set meeting_time='".$meeting_time."' WHERE id='".$assignment_id."'";
+    $result_update = $dbh->query($update);
+
+    fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
+    
+    fclose($myfile);
+    echo "success";
