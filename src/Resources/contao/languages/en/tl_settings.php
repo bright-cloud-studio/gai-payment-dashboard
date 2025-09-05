9d264764965d913ba5ef89a9649640c4a8e66985
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-05 14:33:33 -0400

    Create tl_settings.php

---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2024-2025 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-payment-dashboard
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_member']['pwf_notice_30_day_legend']       = 'Psych Work Form - Notification - 30 Day';
+$GLOBALS['TL_LANG']['tl_member']['pwf_notice_30_day_subject']      = array('Subject', 'Enter the Subject to use for the notification email that is sent out five days before 30-day\'s date if there is no Meeting Date entered');
+$GLOBALS['TL_LANG']['tl_member']['pwf_notice_30_day_body']         = array('Body', 'Enter the Body to use for the notification email that is sent out five days before 30-day\'s date if there is no Meeting Date entered');
