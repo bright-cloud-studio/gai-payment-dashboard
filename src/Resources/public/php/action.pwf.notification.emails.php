33fb6d9dcb9c03f9c21ed95174fe7e3a4f9dbff6
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-05 14:02:34 -0400

    Update action.pwf.notification.emails.php

--- src/Resources/public/php/action.pwf.notification.emails.php
+++ src/Resources/public/php/action.pwf.notification.emails.php
@@ -16,15 +16,74 @@
     // Create a log file so we can track this is working accurately
     $log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../logs/pwf_notification_emails_'.date('m_d_y').'.txt', "a+") or die("Unable to open file!");
 
-    // Loop through all Alert emails
+    // Loop through all Assignments
     $assignments = Assignment::findAll();
     if($assignments) {
         foreach ($assignments as $assignment) {
-            fwrite($log, "Assignment: ID ". $assignment->id ."\r\n");
-            fwrite($log, "Psychologist: ID ". $assignment->psychologist ."\r\n");
-            fwrite($log, "30 Day: ". $assignment->date_30_day ."\r\n");
-            fwrite($log, "Meeting Date: ". $assignment->meeting_date ."\r\n");
-            fwrite($log, "----------------------------------------------------\r\n\r\n");
+            
+            if($assignment->date_30_day) {
+                
+                fwrite($log, "Assignment: ID ". $assignment->id ."\r\n");
+                fwrite($log, "Psychologist: ID ". $assignment->psychologist ."\r\n");
+                
+                $today = date('m/d/y');
+                $date_30_day = $assignment->date_30_day;
+                $date_notice_day = date('m/d/y', strtotime('-5 days', strtotime($date_30_day)));
+                
+                fwrite($log, "Today: ". $today ."\r\n");
+                fwrite($log, "30 Day: ". $date_30_day ."\r\n");
+                fwrite($log, "Notice Day: ". $date_notice_day ."\r\n");
+                
+                
+                if($today == $date_notice_day) {
+                    fwrite($log, "TODAY is 5 days before 30 day! \r\n");
+                    
+                    if(!$assignment->meeting_date) {
+                        fwrite($log, "NO MEETING DATE - SEND NOTIFICATION! \r\n");
+                        
+                        //$addr = $psychologist->email;
+                        $addr = 'mark@brightcloudstudio.com';
+            			$headers = "MIME-Version: 1.0" . "\r\n";
+            			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
+            			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
+            			$sub = "[GA INC. Dashboard] Empty 'Meeting Date' for an Assignment notification";
+            			$message = "
+            				<html>
+            				<head>
+            				<title>Global Assessments, Inc. - Psych Work Form Notification</title>
+            				</head>
+            				<body>".
+            				
+            				"<p>USERNAME,</p>".
+            				
+            				"<p>You have an Assignment on your Psych Work Form that has no 'Meeting Date' filled in. Here are the details:<p>".
+            				
+            				"<p>Assignment ID: $assignment->id</p>".
+            				"<p>District: $assignment->district</p>".
+            				"<p>School: $assignment->school</p>".
+            				"<p>Date Created: $assignment->date_created</p>".
+            				"<p>Date 30 Day: $assignment->date_30_day</p>"
+            				
+                            ."</body>
+            				</html>
+            				";
+
+                        // TEMPLATE TAGS
+                        $message = str_replace('$firstname', $psychologist->firstname, $message);
+                        $message = str_replace('$lastname', $psychologist->lastname, $message);
+                        
+            			mail($addr, $sub, $message, $headers);
+                        
+                    }
+                    
+                }
+                
+                
+                fwrite($log, "Meeting Date: ". $assignment->meeting_date ."\r\n");
+                fwrite($log, "----------------------------------------------------\r\n\r\n");
+            }
+            
         }
     }
 
