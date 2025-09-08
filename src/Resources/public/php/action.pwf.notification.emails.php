1667bcc43440c934af039949f80313cd2f784298
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-08 13:20:33 -0400

    Update action.pwf.notification.emails.php

--- src/Resources/public/php/action.pwf.notification.emails.php
+++ src/Resources/public/php/action.pwf.notification.emails.php
@@ -1,111 +1,187 @@
 <?php
 
-    use Bcs\Model\Assignment;
-    use Bcs\Model\District;
-    use Bcs\Model\School;
-    use Bcs\Model\Student;
-    use Contao\Config;
-    use Contao\MemberModel;
+use Bcs\Model\Assignment;
+use Bcs\Model\District;
+use Bcs\Model\School;
+use Bcs\Model\Student;
+use Contao\Config;
+use Contao\MemberModel;
 
-    // Initialize Session, include Composer
-    session_start();
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
-    
-    // Connect to DB
-    $dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
-    if ($dbh->connect_error) {
-        die("Connection failed: " . $dbh->connect_error);
-    }
+// Initialize Session, include Composer
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
 
-    // Create a log file so we can track this is working accurately
-    $log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../logs/pwf_notification_emails_'.date('m_d_y').'.txt', "a+") or die("Unable to open file!");
+// Connect to DB
+$dbh = new mysqli("localhost", "staging_user", 'q&U,zA(+WUK$kQ!cZB', "staging_contao_5_3");
+if ($dbh->connect_error) {
+    die("Connection failed: " . $dbh->connect_error);
+}
 
-    // Loop through all Assignments
-    $assignments = Assignment::findAll();
-    if($assignments) {
-        foreach ($assignments as $assignment) {
+// Create a log file so we can track this is working accurately
+$log = fopen($_SERVER['DOCUMENT_ROOT'] . '/../logs/pwf_notification_emails_'.date('m_d_y').'.txt', "a+") or die("Unable to open file!");
+
+// Loop through all Assignments
+$assignments = Assignment::findAll();
+if($assignments) {
+    foreach ($assignments as $assignment) {
+
+        ///////////////////////////
+        // NOTIFICATION - 30 DAY //
+        ///////////////////////////
+        
+        if($assignment->date_30_day && 1==2) {
+
+            // Get Today's, 30-day's and five days before 30-day's dates
+            $today = date('m/d/y');
+            $date_30_day = $assignment->date_30_day;
+            $date_notice_day = date('m/d/y', strtotime('-5 days', strtotime($date_30_day)));
             
-            if($assignment->date_30_day) {
-
-                // Get Today's, 30-day's and five days before 30-day's dates
-                $today = date('m/d/y');
-                $date_30_day = $assignment->date_30_day;
-                $date_notice_day = date('m/d/y', strtotime('-5 days', strtotime($date_30_day)));
+            // If today is the same as five days before 30 day
+            if($today == $date_notice_day) {
                 
-                // If today is the same as five days before 30 day
-                if($today == $date_notice_day) {
+                // Write in our log, it is Notification Day for this Assignment
+                fwrite($log, "NOTICE DAY FOR 30-DAY \r\n");
+                fwrite($log, "Assignment: ID ". $assignment->id ."\r\n");
+                fwrite($log, "Psychologist: ID ". $assignment->psychologist ."\r\n");
+                fwrite($log, "Today: ". $today ."\r\n");
+                fwrite($log, "30 Day: ". $date_30_day ."\r\n");
+                fwrite($log, "Notice Day: ". $date_notice_day ."\r\n");
+                fwrite($log, "TODAY is 5 days before 30 day! \r\n");
+                
+                // If we have no 'Meeting Date'
+                if(!$assignment->meeting_date) {
                     
-                    // Write in our log, it is Notification Day for this Assignment
-                    fwrite($log, "Assignment: ID ". $assignment->id ."\r\n");
-                    fwrite($log, "Psychologist: ID ". $assignment->psychologist ."\r\n");
-                    fwrite($log, "Today: ". $today ."\r\n");
-                    fwrite($log, "30 Day: ". $date_30_day ."\r\n");
-                    fwrite($log, "Notice Day: ". $date_notice_day ."\r\n");
-                    fwrite($log, "TODAY is 5 days before 30 day! \r\n");
+                    fwrite($log, "NO MEETING DATE - SEND NOTIFICATION! \r\n");
                     
-                    // If we have no 'Meeting Date'
-                    if(!$assignment->meeting_date) {
-                        
-                        fwrite($log, "NO MEETING DATE - SEND NOTIFICATION! \r\n");
-                        
-                        $psychologist = MemberModel::findOneBy('id', $assignment->psychologist);
-                        $district = District::findOneBy('id', $assignment->district);
-                        $school = School::findOneBy('id', $assignment->school);
-                        $student = Student::findOneBy('id', $assignment->student);
-                        
-                        //$addr = $psychologist->email;
-                        $addr = 'mark@brightcloudstudio.com';
-            			$headers = "MIME-Version: 1.0" . "\r\n";
-            			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-            			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
-            			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
-            			$sub = Config::get('pwf_notice_30_day_subject'); 
-            			$message = "
-            				<html>
-            				<head>
-            				<title>Global Assessments, Inc. - Psych Work Form - Notice - 30 Day</title>
-            				</head>
-            				<body>".
-                                Config::get('pwf_notice_30_day_body')
-                            ."</body>
-            				</html>
-            				";
+                    $psychologist = MemberModel::findOneBy('id', $assignment->psychologist);
+                    $district = District::findOneBy('id', $assignment->district);
+                    $school = School::findOneBy('id', $assignment->school);
+                    $student = Student::findOneBy('id', $assignment->student);
+                    
+                    //$addr = $psychologist->email;
+                    $addr = 'mark@brightcloudstudio.com';
+        			$headers = "MIME-Version: 1.0" . "\r\n";
+        			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
+        			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
+        			$sub = Config::get('pwf_notice_30_day_subject'); 
+        			$message = "
+        				<html>
+        				<head>
+        				<title>Global Assessments, Inc. - Psych Work Form - Notice - 30 Day</title>
+        				</head>
+        				<body>".
+                            Config::get('pwf_notice_30_day_body')
+                        ."</body>
+        				</html>
+        				";
 
-                        // TEMPLATE TAGS - Psychologist
-                        $message = str_replace('$firstname', $psychologist->firstname, $message);
-                        $message = str_replace('$lastname', $psychologist->lastname, $message);
-                        // TEMPLATE TAGS - Assignment
-                        $message = str_replace('$date_created', date('m/d/y', $assignment->date_created), $message);
-                        $message = str_replace('$date_30_day', $assignment->date_30_day, $message);
-                        // TEMPLATE TAGS - District
-                        $message = str_replace('$district', $district->district_name, $message);
-                        // TEMPLATE TAGS - School
-                        $message = str_replace('$school', $school->school_name, $message);
-                        // TEMPLATE TAGS - DEV
-                        $message = str_replace('$psy_email', $psychologist->email, $message);
-                        // TEMPLATE TAGS - Student Initials
-                        $message = str_replace('$student_initials', getInitials($student->name), $message);
-                        
-                        
-            			mail($addr, $sub, $message, $headers);
-                        
-                    }
+                    // TEMPLATE TAGS - Psychologist
+                    $message = str_replace('$firstname', $psychologist->firstname, $message);
+                    $message = str_replace('$lastname', $psychologist->lastname, $message);
+                    // TEMPLATE TAGS - Assignment
+                    $message = str_replace('$date_created', date('m/d/y', $assignment->date_created), $message);
+                    $message = str_replace('$date_30_day', $assignment->date_30_day, $message);
+                    // TEMPLATE TAGS - District
+                    $message = str_replace('$district', $district->district_name, $message);
+                    // TEMPLATE TAGS - School
+                    $message = str_replace('$school', $school->school_name, $message);
+                    // TEMPLATE TAGS - Student Initials
+                    $message = str_replace('$student_initials', getInitials($student->name), $message);
+                    
+                    
+        			mail($addr, $sub, $message, $headers);
                     
                 }
                 
-                
-                fwrite($log, "Meeting Date: ". $assignment->meeting_date ."\r\n");
-                fwrite($log, "----------------------------------------------------\r\n\r\n");
             }
             
+            
+            fwrite($log, "Meeting Date: ". $assignment->meeting_date ."\r\n");
+            fwrite($log, "----------------------------------------------------\r\n\r\n");
         }
+
+        ///////////////////////////
+        // NOTIFICATION - 30 DAY //
+        ///////////////////////////
+        if($assignment->meeting_date) {
+
+            // Get Today's, 30-day's and five days before 30-day's dates
+            $today = date('m/d/y');
+            $meeting_date = $assignment->meeting_date;
+            $date_notice_day = date('m/d/y', strtotime('-4 days', strtotime($meeting_date)));
+            
+            if($today == $date_notice_day) {
+                
+                fwrite($log, "TODAY is 4 days before Meeting Date! \r\n");
+                
+                if($assignment->report_submitted != 'yes') {
+                    
+                    fwrite($log, "NOTICE DAY FOR REPORT SUBMITTED \r\n");
+                    fwrite($log, "Assignment: ID ". $assignment->id ."\r\n");
+                    fwrite($log, "Psychologist: ID ". $assignment->psychologist ."\r\n");
+                    fwrite($log, "Today: ". $today ."\r\n");
+                    fwrite($log, "Meeting Date: ". $meeting_date ."\r\n");
+                    fwrite($log, "Notice Day: ". $date_notice_day ."\r\n");
+                    fwrite($log, "NO REPORT SUBMITTED - SEND NOTIFICATION! \r\n");
+                    
+                    $psychologist = MemberModel::findOneBy('id', $assignment->psychologist);
+                    $district = District::findOneBy('id', $assignment->district);
+                    $school = School::findOneBy('id', $assignment->school);
+                    $student = Student::findOneBy('id', $assignment->student);
+                    
+                    //$addr = $psychologist->email;
+                    $addr = 'mark@brightcloudstudio.com';
+        			$headers = "MIME-Version: 1.0" . "\r\n";
+        			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        			$headers .= 'From: <billing@globalassessmentsinc.com>' . "\r\n";
+        			$headers .= 'Cc: ed@globalassessmentsinc.com' . "\r\n";
+        			$sub = Config::get('pwf_notice_report_submitted_subject'); 
+        			$message = "
+        				<html>
+        				<head>
+        				<title>Global Assessments, Inc. - Psych Work Form - Notice - Report Submitted</title>
+        				</head>
+        				<body>".
+                            Config::get('pwf_notice_report_submitted_body')
+                        ."</body>
+        				</html>
+        				";
+
+                    // TEMPLATE TAGS - Psychologist
+                    $message = str_replace('$firstname', $psychologist->firstname, $message);
+                    $message = str_replace('$lastname', $psychologist->lastname, $message);
+                    // TEMPLATE TAGS - Assignment
+                    $message = str_replace('$date_created', date('m/d/y', $assignment->date_created), $message);
+                    $message = str_replace('$meeting_date', $assignment->meeting_date, $message);
+                    // TEMPLATE TAGS - District
+                    $message = str_replace('$district', $district->district_name, $message);
+                    // TEMPLATE TAGS - School
+                    $message = str_replace('$school', $school->school_name, $message);
+                    // TEMPLATE TAGS - Student Initials
+                    $message = str_replace('$student_initials', getInitials($student->name), $message);
+                    
+                    
+        			mail($addr, $sub, $message, $headers);
+                
+                }
+                
+                fwrite($log, "----------------------------------------------------\r\n\r\n");
+            }
+
+        }
+        
     }
+}
 
-    // Close our log file
-    fclose($log);
+// Close our log file
+fclose($log);
 
 
-
+// Simple custom function to take in a $name as a string and return back the capital letters.
+// Explode will split the $name string into separate words using space as a divider
+// strtoupper will strip out all characters for each word extept the first character
+// leaving us with only the initials
 function getInitials($name) {
     $words = explode(" ", $name);
     $initials = "";
