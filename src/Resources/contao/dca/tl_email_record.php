e7bda13ad56f66b313b6f61bfa71791e051b9f17
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-10 12:53:05 -0400

    Update tl_email_record.php

--- src/Resources/contao/dca/tl_email_record.php
+++ src/Resources/contao/dca/tl_email_record.php
@@ -30,18 +30,18 @@ $GLOBALS['TL_DCA']['tl_email_record'] = array
         'sorting' => array
         (
             // Attempt to list as collapsable view
-            'mode'                    => DataContainer::MODE_SORTED,
+            'mode'                    => DataContainer::MODE_UNSORTED,
             'rootPaste'               => false,
             'showRootTrails'          => false,
             'icon'                    => 'pagemounts.svg',
             'flag'                    => DataContainer::SORT_DESC,
-            'fields'                  => array('date_created'),
+            'fields'                  => array('id'),
             'panelLayout'             => 'sort,filter;search,limit'
         ),
         'label' => array
         (
-            'fields'                  => array('date_created', 'email_type'),
-            'format'                  => '%s - %s',
+            'fields'                  => array('email_type'),
+            'format'                  => '%s',
             'label_callback'          => array('tl_email_record', 'generateLabel')
         ),
         'global_operations' => array
@@ -177,6 +177,7 @@ class tl_email_record extends Backend
 {
     public function generateLabel($row, $label, DataContainer|null $dc=null, $imageAttribute='', $blnReturnImage=false, $blnProtected=false, $isVisibleRootTrailPage=false)
     {
+        $label = $row['date_created'] . " - " . $label;
         return Backend::addPageIcon($row, $label, $dc, $imageAttribute, $blnReturnImage, $blnProtected, $isVisibleRootTrailPage);
     }
 }
