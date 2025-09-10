0080fc228480d3226851072a167fdd78fcc0335e
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-10 11:51:35 -0400

    Create tl_email_record.php

---
+++ src/Resources/contao/languages/en/tl_email_record.php
@@ -0,0 +1,17 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_email_record']['new']                        = array('New Email Record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_email_record']['show']                       = array('Email Record Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_email_record']['edit']                       = array('Edit Email Record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_email_record']['copy']                       = array('Copy Email Record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_email_record']['delete']                     = array('Delete Email Record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_email_record']['toggle']                     = array('Toggle Email Record', 'Toggle record ID %s');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_email_record']['email_record_legend']      = 'Email Record Information';
+$GLOBALS['TL_LANG']['tl_email_record']['date_created']             = array('Date Created', 'The date and time this email and record were created');
+$GLOBALS['TL_LANG']['tl_email_record']['email_type']               = array('Email Type', 'The specific type of email that was sent out');
+$GLOBALS['TL_LANG']['tl_email_record']['email_recipient']          = array('Email Recipient', 'The recipient of this email');
+$GLOBALS['TL_LANG']['tl_email_record']['email_subject']            = array('Email Subject', 'The subject of this email');
+$GLOBALS['TL_LANG']['tl_email_record']['email_body']               = array('Email Body', 'The body of this email');
