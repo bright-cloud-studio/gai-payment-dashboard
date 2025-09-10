deb3eea23a946b26a03c753fbe701c4e913971bb
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-10 11:56:58 -0400

    Update EmailRecordBackend.php

--- src/Resources/contao/library/Bcs/Backend/EmailRecordBackend.php
+++ src/Resources/contao/library/Bcs/Backend/EmailRecordBackend.php
@@ -12,23 +12,34 @@ use Contao\StringUtil;
 class EmailRecordBackend extends Backend
 {
   
-	public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
-	{
-      if (strlen(Input::get('tid')))
-      {
-        $this->toggleVisibility(Input::get('tid'), (Input::get('state') == 1), (@func_get_arg(12) ?: null));
-        $this->redirect($this->getReferer());
-      }
+    public function toggleIcon($row, $href, $label, $title, $icon, $attributes)
+    {
+        if (strlen(Input::get('tid')))
+        {
+            $this->toggleVisibility(Input::get('tid'), (Input::get('state') == 1), (@func_get_arg(12) ?: null));
+            $this->redirect($this->getReferer());
+        }
+        $href .= '&amp;tid='.$row['id'].'&amp;state='.($row['published'] ? '' : 1);
+        if (!$row['published'])
+        {
+            $icon = 'invisible.gif';
+        }
+        return '<a href="'.$this->addToUrl($href).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ';
+    }
 
-  		$href .= '&amp;tid='.$row['id'].'&amp;state='.($row['published'] ? '' : 1);
-  
-  		if (!$row['published'])
-  		{
-  			$icon = 'invisible.gif';
-  		}
+    public function getPsychologists(DataContainer $dc) { 
 
-		  return '<a href="'.$this->addToUrl($href).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ';
-	}	
+        $psychologists = array();
+
+		$this->import('Database');
+		$result = $this->Database->prepare("SELECT * FROM tl_member WHERE disable=0 ORDER BY firstname ASC")->execute();
+		while($result->next())
+		{
+            $psychologists = $psychologists + array($result->id => ($result->firstname . " " . $result->lastname));   
+		}
+
+		return $psychologists;
+	}
 
 
 }
