304bcc13e170023b4d2e70f10424db6993d42656
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-25 12:44:55 -0400

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -2,7 +2,7 @@
 
 use Contao\Config;
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{date_legend', '{pwf_notice_30_day_legend}, pwf_notice_30_day_subject, pwf_notice_30_day_body;{pwf_notice_report_submitted_legend}, pwf_notice_report_submitted_subject, pwf_notice_report_submitted_body;{date_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{date_legend', '{pwf_notice_30_day_legend}, pwf_notice_30_day_subject, pwf_notice_30_day_body;{pwf_notice_report_submitted_legend}, pwf_notice_report_submitted_subject, pwf_notice_report_submitted_body;{pwf_hide_legend},pwf_hide_cutoff;{date_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
@@ -41,6 +41,17 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
         'filter'                  => false,
         'eval'                    => array('mandatory'=>false, 'tl_class'=>'clr w100', 'rte'=>'tinyMCE', 'allowHtml' => true),
         'sql'                     => "text NOT NULL default ''"
+    ],
+
+    
+    'pwf_hide_cutoff' => [
+        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['pwf_hide_cutoff'],
+        'inputType'               => 'text',
+        'default'                 => '',
+        'search'                  => false,
+        'filter'                  => false,
+        'eval'                    => array('rgxp'=>'date', 'datepicker'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
+        'sql'                     => "varchar(20) NOT NULL default ''",
     ],
 
 ];
