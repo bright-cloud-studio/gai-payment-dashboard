6997cbc3b3ed89b5767e4ed841c1d8e0c965fe22
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-26 13:58:11 -0400

    Update tl_email_record.php

--- src/Resources/contao/dca/tl_email_record.php
+++ src/Resources/contao/dca/tl_email_record.php
@@ -184,7 +184,7 @@ class tl_email_record extends Backend
     public function generateLabel($row, $label, DataContainer|null $dc=null, $imageAttribute='', $blnReturnImage=false, $blnProtected=false, $isVisibleRootTrailPage=false)
     {
         // Prepend the label with the date, formatted correctly
-        $label = '<span clas=="status status_'.$row['status'].'">[' . $row['status'] . ']</span> ' . date('m/d/y g:i a', $row['date_created']) . " - " . $label;
+        $label = '<span class="status status_'.$row['status'].'">[' . $row['status'] . ']</span> ' . date('m/d/y g:i a', $row['date_created']) . " - " . $label;
         return $label;
     }
 }
