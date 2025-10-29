9b01ddc7bf4aaa62010b4690ac9a1fe9adcfcaa3
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-29 10:36:08 -0400

    Set default value of 'published' to '1'

--- src/Resources/contao/dca/tl_district.php
+++ src/Resources/contao/dca/tl_district.php
@@ -137,8 +137,9 @@ $GLOBALS['TL_DCA']['tl_district'] = array
             'exclude'                 => true,
             'label'                   => &$GLOBALS['TL_LANG']['tl_district']['published'],
             'inputType'               => 'checkbox',
+            'default'                 => '1',
             'eval'                    => array('submitOnChange'=>false, 'doNotCopy'=>true),
-            'sql'                     => "char(1) NOT NULL default ''"
+            'sql'                     => "char(1) NOT NULL default '1'"
         ),
 
 
