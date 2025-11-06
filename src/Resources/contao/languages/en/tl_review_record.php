380b9df51da96d5a2d9674772bff7f4ef7686f09
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-11-06 15:20:38 -0500

    Create tl_review_record.php

---
+++ src/Resources/contao/languages/en/tl_review_record.php
@@ -0,0 +1,18 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_review_record']['new']                        = array('New Review Record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_review_record']['show']                       = array('Review Record Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_review_record']['edit']                       = array('Edit Review Record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_review_record']['copy']                       = array('Copy Review Record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_review_record']['delete']                     = array('Delete Review Record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_review_record']['toggle']                     = array('Toggle Review Record', 'Toggle record ID %s');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_review_record']['review_record_legend']       = 'Review Record Details';
+$GLOBALS['TL_LANG']['tl_review_record']['date_reviewed']              = array('Date Reviewed', 'The date this record was created and the date the Psychologist submitted a Review');
+$GLOBALS['TL_LANG']['tl_review_record']['psychologist']               = array('Psychologist', 'The Psycholohist this record is for');
+$GLOBALS['TL_LANG']['tl_review_record']['total_assignments']          = array('Total Assignments', 'Total number of Assignments this Psychologist had at the time of this Review\'s submission');
+$GLOBALS['TL_LANG']['tl_review_record']['total_transactions']         = array('Total Transactions', 'Total number of Transactions this Psycholohist has at the time of this Review\'s submission');
+$GLOBALS['TL_LANG']['tl_review_record']['total_reviewed']             = array('Total Reviewed', 'Total number of Reviewed Transactions this Psychologist has at the time of this Review\'s submission');
+$GLOBALS['TL_LANG']['tl_review_record']['percentage_reviewed']        = array('Percentage Reviewed', 'Percentage of Reviewed Transactions against the total number of Transactions');
