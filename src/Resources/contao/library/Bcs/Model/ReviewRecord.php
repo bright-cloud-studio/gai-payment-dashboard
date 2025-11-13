2c0381a040a661209add15b0fe392a157adc1a50
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-11-13 12:05:50 -0500

    Create ReviewRecord.php

---
+++ src/Resources/contao/library/Bcs/Model/ReviewRecord.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace Bcs\Model;
+
+use Contao\Model;
+
+class ReviewRecord extends Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_review_record';
+    
+}
