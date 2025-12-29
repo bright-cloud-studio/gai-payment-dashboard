f1a001d03183da0a4278ed85b3a0f4428f70c4e2
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-12-29 12:36:49 -0500

    Refactor Select2 initialization comments

--- src/Resources/public/js/backend.js
+++ src/Resources/public/js/backend.js
@@ -1,11 +1,9 @@
-/* Once fully loaded */
 $(document).ready(function() {
 
-    /* Apply Select2 - Assignments */
+    /* Assignments - Initialize Select2 */
     $(document).ready(function(){ $('#ctrl_psychologist').select2(); });
     $(document).ready(function(){ $('#ctrl_school').select2(); });
     $(document).ready(function(){ $('#ctrl_student_gender').select2(); });
-
     $(document).ready(function(){ $('#ctrl_initial_reeval').select2(); });
     $(document).ready(function(){ $('#ctrl_type_of_testing').select2(); });
     $(document).ready(function(){ $('#ctrl_meeting_required').select2(); });
