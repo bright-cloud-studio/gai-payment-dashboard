2c820b5862fd4d2056590b0cce44e33ce3f4709c
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-12-30 13:40:07 -0500

    Update tl_assignment.php

--- src/Resources/contao/dca/tl_assignment.php
+++ src/Resources/contao/dca/tl_assignment.php
@@ -157,7 +157,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'filter'                  => true,
             'search'                  => true,
             'eval'                    => array('rgxp'=>'date', 'datepicker'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
+            'sql'                     => "int(10) unsigned NOT NULL default 0",
             'default'                => time(),
         ),
         'date_30_day' => array
