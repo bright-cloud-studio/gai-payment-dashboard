90e7e27e66c4af9e760f8f46dcbdea4bb305b6a0
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-22 10:43:00 -0500

    Update action.save.contact_info_teacher.php

--- src/Resources/public/php/action.save.contact_info_teacher.php
+++ src/Resources/public/php/action.save.contact_info_teacher.php
@@ -15,11 +15,12 @@
     // Get data from ajax
     $assignment_id = $_POST['assignment_id'];
     $contact_info_teacher = $_POST['contact_info_teacher'];
+    $contact_info_teacher_cleaned = htmlspecialchars($contact_info_teacher, ENT_QUOTES, 'UTF-8');
     
     fwrite($myfile, "SAVING: Assignment ID: " . $assignment_id . "\r\n");
-    fwrite($myfile, "SAVING: Contact Info Teacher: " . $contact_info_teacher . "\r\n");
+    fwrite($myfile, "SAVING: Contact Info Teacher: " . $contact_info_teacher_cleaned . "\r\n");
 
-    $update =  "update tl_assignment set contact_info_teacher='".$contact_info_teacher."' WHERE id='".$assignment_id."'";
+    $update =  "update tl_assignment set contact_info_teacher='".$contact_info_teacher_cleaned."' WHERE id='".$assignment_id."'";
     $result_update = $dbh->query($update);
 
     fwrite($myfile, "SAVING: Query Results: " . $result_update . "\r\n");
