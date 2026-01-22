030317b0cee8c2977a6d05fa075078e3928cc4d8
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-22 14:46:57 -0500

    Update chart.get.assignment.data.by.year.php

--- src/Resources/public/php/chart.get.assignment.data.by.year.php
+++ src/Resources/public/php/chart.get.assignment.data.by.year.php
@@ -18,14 +18,12 @@
     // Generate Month labels
     $month_labels = [];
     $month = date('n');
-    $month = 12;
     for ($i = 1; $i <= $month; $i++) {
         $month_labels[] = date('M', mktime(0, 0, 0, $i, 1));
     }
     
     // Get the Year range we are requesting data for
     $year = date('y');
-    $year = 25;
     
     // Get all Services
     $services = array();
@@ -33,21 +31,24 @@
     $s_r = $dbh->query($s_q);
     if($s_r) {
         while($s = $s_r->fetch_assoc()) {
-            $services[$s['service_code']] = $s['name'];
+            $services[$s['service_code']]['service_name'] = $s['name'];
+            $services[$s['service_code']]['graph_color'] = $s['graph_color'];
         }
     }
+    
     
     // Stage Assignment data. Store by: Service Code > Month > Total Usage
     $assignments = array();
-    $a_q = "SELECT * FROM tl_assignment WHERE RIGHT(date_created, 2) = '".$year."';";
+    $a_q = "SELECT * FROM tl_assignment WHERE date_created LIKE '%/".$year."' AND published='1'";
     $a_r = $dbh->query($a_q);
     if($a_r) {
         while($a = $a_r->fetch_assoc()) {
+
             $month = date('M', strtotime($a['date_created']));
-            $assignments[$a['type_of_testing']][$month] += 1;
+            $assignments[$a['type_of_testing']][$month] = ($assignments[$a['type_of_testing']][$month] ?? 0) + 1;
         }
     }
-    
+
     
     $datasets = [];
     // Loop through staged Assignment data and stage Chartjs data
@@ -59,11 +60,15 @@
             $totals_by_month[] = $total_usage;
         }
         
+        $label = "N/A";
+        if($service_code != 0)
+            $label = $services[$service_code]['service_name'];
+        
         $datasets[] = [
-            'label'           => $services[$service_code],
+            'label'           => $label,
             'type'            => "bar",
             'data'            => $totals_by_month,
-            'backgroundColor' => "rgba(".rand(1, 255).",".rand(1, 255).",".rand(1, 255).", 0.7)",
+            'backgroundColor' => hex2rgba($services[$service_code]['graph_color'], 0.7),
             'yAxisID'         => "yCount"
         ];
         
@@ -76,3 +81,18 @@
         'datasets' =>
             $datasets
     ]);
+
+    /* Convert our hexidecimal color to RGBA */
+    function hex2rgba($hex, $alpha = 1.0) {
+        $hex = str_replace("#", "", $hex);
+        if (strlen($hex) == 3) {
+            $r = hexdec(str_repeat(substr($hex, 0, 1), 2));
+            $g = hexdec(str_repeat(substr($hex, 1, 1), 2));
+            $b = hexdec(str_repeat(substr($hex, 2, 1), 2));
+        } else {
+            $r = hexdec(substr($hex, 0, 2));
+            $g = hexdec(substr($hex, 2, 2));
+            $b = hexdec(substr($hex, 4, 2));
+        }
+        return "rgba($r, $g, $b, $alpha)";
+    }
