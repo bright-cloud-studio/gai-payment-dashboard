e1c176c6d27d454d1fa69a41d4d62b399d91016b
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-03 12:19:53 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -10,9 +10,6 @@ use Contao\StringUtil;
 /* Override the switch user function with our custom one that routes the user to the Dashboard instead of the homepage */
 $GLOBALS['TL_DCA']['tl_member']['list']['operations']['su']['button_callback'] = array('Bcs\Backend\MemberBackend', 'switchUserCustomized');
 
-/* Add a confirmation prompt for toggling */
-$GLOBALS['TL_DCA']['tl_member']['list']['operations']['toggle']['attributes'] = 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['confirmToggle'] ?? 'Please confirm you would like to toggle this Member on/off') . '\')) return false; Backend.getScrollOffset()"';
-
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
     $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend}, price_tier, price_tier_display;{admin_review_legend}, last_reviewed; {last_review_and_submit_legend}, last_review_and_submit; {pwf_hidden_assignments_legend}, pwf_hidden_assignments;', $v);
